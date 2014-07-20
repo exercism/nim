0f0136fc5da34aaba5ae8b9198cc1065b5d713cf
Simon Jakobi <simon.jakobi@gmail.com>
2014-07-20 02:36:39 +0200

    Add bob exercise

---
+++ bob/bob_test.nim
@@ -0,0 +1,63 @@
+import unittest
+from bob import hey
+
+suite "hey tests":
+  test "stating something":
+    check hey("Tom-ay-to, tom-aaaah-to.") == "Whatever."
+
+  test "shouting":
+    check hey("WATCH OUT!") == "Woah, chill out!"
+
+  test "asking a question":
+    check hey("Does this cryogenic chamber make me look fat?") == "Sure."
+
+  test "asking a numeric question":
+    check hey("You are, what, like 15?") == "Sure."
+
+  test "talking forcefully":
+    check hey("Let's go make out behind the gym!") == "Whatever."
+
+  test "using acronyms in regular speech":
+    check hey("It's OK if you don't want to go to the DMV.") == "Whatever."
+
+  test "forceful questions":
+    check hey("WHAT THE HELL WERE YOU THINKING?") == "Woah, chill out!"
+
+  test "shouting numbers":
+    check hey("1, 2, 3 GO!") == "Woah, chill out!"
+
+  test "only numbers":
+    check hey("1, 2, 3") == "Whatever."
+
+  test "question with only numbers":
+    check hey("4?") == "Sure."
+
+  test "shouting with special characters":
+    check hey("ZOMG THE %^*@#$(^ ZOMBIES ARE COMING!!1!") == "Woah, chill out!"
+
+  test "shouting with umlauts":
+    check hey("ÜMLÄÜTS!") == "Woah, chill out!"
+
+  test "calmly speaking with umlauts":
+    check hey("Ümläüts!") == "Whatever."
+
+  test "shouting with no exclamation mark":
+    check hey("I HATE YOU") == "Woah, chill out!"
+
+  test "statement containing question mark":
+    check hey("Ending with ? means a question.") == "Whatever."
+
+  test "prattling on":
+    check hey("Wait! Hang on. Are you going to be OK?") == "Sure."
+
+  test "silence":
+    check hey("") == "Fine. Be that way!"
+
+  test "prolonged silence":
+    check hey("    ") == "Fine. Be that way!"
+
+  test "really prolonged silence":
+    check hey("              \t\n") == "Fine. Be that way!"
+
+  test "multi line trick question":
+    check hey("Do I ever change my mind?\nNo.") == "Whatever."
---
+++ bob/example.nim
@@ -0,0 +1,45 @@
+from strutils import endsWith
+from unicode import isLower, isUpper, isWhiteSpace, runes
+  
+proc isSilence(s: string): bool =
+  for r in runes(s):
+    if not isWhiteSpace(r):
+      return false
+  return true
+
+proc isYelling(s: string): bool =
+  var nUpperRunes = 0
+  for r in runes(s):
+    if isLower(r):
+      return false
+    elif isUpper(r):
+      inc(nUpperRunes)
+  return nUpperRunes > 0
+
+proc isQuestion(s: string): bool =
+  return s.endsWith("?")
+
+proc hey*(msg: string): string {.noSideEffect.} =
+  ## Returns the response of a lackadaisical teenager to a futile attempt to
+  ## communicate with him.
+  ##
+  ## .. code-block:: nimrod
+  ##   let greeting = "Hi, Bob!"
+  ##   assert hey(greeting) == "Whatever."
+  ##
+  ##   let question == "What's up, Bob?"
+  ##   assert hey(question) == "Sure."
+  ##
+  ##   let rage = "TALK TO ME!!!"
+  ##   assert hey(rage) == "Woah, chill out!"
+  ##
+  ##   let resignation = ""
+  ##   assert hey(resignation) == "Fine. Be that way!"
+  if isSilence(msg):
+    return "Fine. Be that way!"
+  elif isYelling(msg):
+    return "Woah, chill out!"
+  elif isQuestion(msg):
+    return "Sure."
+  else:
+    return "Whatever."
