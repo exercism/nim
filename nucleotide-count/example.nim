e739c4e550ae9e00d50bac1abf878238b3bf8b6e
Jeffrey Pratt <clementi@users.noreply.github.com>
2016-09-07 10:32:58 -0600

    Add nucleotide-count exercise

---
+++ nucleotide-count/example.nim
@@ -0,0 +1,23 @@
+import sequtils
+import tables
+
+proc count*(nt: char, dna: string): int =
+  let nts = toSeq(dna.items)
+  var nt_count = 0
+
+  for n in nts:
+    if n == nt:
+      nt_count += 1
+
+  nt_count
+
+proc count_dna*(dna: string): CountTable[char] =
+  var counts = initCountTable[char]()
+
+  for nt in toSeq(dna.items):
+    if nt in ['A', 'C', 'G', 'T']:
+      counts.inc(nt)
+    else:
+      raise newException(ValueError, "Invalid nucleotide")
+
+  counts
