4902a5e37ffc58497ca15140776ab078364286a4
Anthony Scotti <anthony.m.scotti@gmail.com>
2018-05-17 19:23:55 -0400

    Adds two-fer exercise

---
+++ exercises/two-fer/two_fer_test.nim
@@ -0,0 +1,16 @@
+import unittest
+
+import two_fer
+
+suite "Two Fer":
+
+  test "no name given":
+    check twoFer() == "One for you, one for me."
+
+  test "a name given":
+    let name = "Alice"
+    check twoFer(name) == "One for Alice, one for me."
+
+  test "another name given":
+    let name = "Bob"
+    check twoFer(name) == "One for Bob, one for me."
