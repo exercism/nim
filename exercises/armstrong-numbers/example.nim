a69841c3f2f56e5cf6c9fb029d4da740ed61436d
Anthony Scotti <anthony.m.scotti@gmail.com>
2018-05-19 14:24:29 -0400

    Adds armstrong-numbers exercise

---
+++ exercises/armstrong-numbers/example.nim
@@ -0,0 +1,11 @@
+import
+  math, strutils
+
+proc isArmstrongNumber*(input: int): bool =
+  let stringNumber = $input
+  let numberLength = len(stringNumber).toFloat
+
+  var sum = 0
+  for i in stringNumber:
+    sum = sum + pow(parseFloat($i), numberLength).toInt
+  sum == input
