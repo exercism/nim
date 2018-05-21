95455075debede46905cd4fd282b2222f9efd524
Anthony Scotti <anthony.m.scotti@gmail.com>
2018-05-21 18:22:53 -0400

    Updates to leap exercise

--- exercises/leap/leap_test.nim
+++ exercises/leap/leap_test.nim
@@ -1,14 +1,16 @@
 import unittest
 from leap import isLeapYear
 
-test "vanilla leap year":
-  check isLeapYear(1996)
+suite "Leap":
 
-test "normal year":
-  check (not isLeapYear(1997))
-
-test "century":
-  check (not isLeapYear(1900))
-
-test "exceptional century":
-  check isLeapYear(2000)
+  test "year not divisible by 4: common year":
+    check isLeapYear(2015) == false
+   
+  test "year divisible by 4, not divisible by 100: leap year":
+    check isLeapYear(1996) == true
+  
+  test "year divisible by 100, not divisible by 400: common year":
+    check isLeapYear(2100) == false
+  
+  test "year divisible by 400: leap year":
+    check isLeapYear(2000) == true
