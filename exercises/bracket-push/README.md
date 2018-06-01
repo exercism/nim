19c8c6feb910d8bb14133aa3813c28361c1093ba
Anthony Scotti <anthony.m.scotti@gmail.com>
2018-05-31 20:26:45 -0400

    Adds bracket-push exercise

---
+++ exercises/bracket-push/README.md
@@ -0,0 +1,11 @@
+# Bracket Push
+
+Given a string containing brackets `[]`, braces `{}`, parentheses `()`,
+or any combination thereof, verify that any and all pairs are matched
+and nested correctly.
+## Source
+
+Ginna Baker
+
+## Submitting Incomplete Solutions
+It's possible to submit an incomplete solution so you can see how others have completed the exercise.
---
+++ exercises/bracket-push/bracket_push_test.nim
@@ -0,0 +1,52 @@
+import unittest
+
+import bracket_push
+
+suite "bracket-push":
+
+  test "paired square brackets":
+    check isPaired("[]") == true
+
+  test "empty string":
+    check isPaired("") == true
+
+  test "unpaired brackets":
+    check isPaired("[[") == false
+
+  test "wrong ordered brackets":
+    check isPaired("}{") == false
+
+  test "wrong closing bracket":
+    check isPaired("{]") == false
+
+  test "wrong closing bracket":
+    check isPaired("{ }") == true
+
+  test "wrong closing bracket":
+    check isPaired("{[])") == false
+
+  test "simple nested brackets":
+    check isPaired("{[]}") == true
+
+  test "several paired brackets":
+    check isPaired("{}[]") == true
+
+  test "paired and nested brackets":
+    check isPaired("([{}({}[])])") == true
+
+  test "unopened closing brackets":
+    check isPaired("{[)][]}") == false
+
+  test "unpaired and nested brackets":
+    check isPaired("([{])") == false
+
+  test "paired and wrong nested brackets":
+    check isPaired("[({]})") == false
+
+  test "math expression":
+    check isPaired("(((185 + 223.85) * 15) - 543)/2") == true
+
+  test "complex latex expression":
+    let value = "\\left(\\begin{array}{cc} \\frac{1}{3} & x\\\\ \\mathrm{e}^{x} &... x^2 \\end{array}\\right)"
+    
+    check isPaired(value) == true
