3c2344268685ff88630e4590be0b4b511183b5f8
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:23 +0100

    queen-attack: Wrap tests with a unittest suite

--- exercises/queen-attack/queen_attack_test.nim
+++ exercises/queen-attack/queen_attack_test.nim
@@ -1,64 +1,64 @@
 import unittest
-
 import queen_attack
 
-# Raise an error for invalid positions
-test "invalid position in canAttack":
-  expect(ValueError):
-    discard canAttack((0, 0), (7, 8))
+suite "Queen Attack":
+  # Raise an error for invalid positions
+  test "invalid position in canAttack":
+    expect(ValueError):
+      discard canAttack((0, 0), (7, 8))
 
 
-# Test the ability of one queen to attack another
-test "true attacks":
-  var positions: seq[tuple[white, black: tuple[rank, file: int]]] = @[((2, 3), (5, 6)),
-                                                                      ((2, 6), (5, 3)),
-                                                                      ((2, 4), (2, 7)),
-                                                                      ((5, 4), (2, 4)),
-                                                                      ((1, 1), (6, 6)),
-                                                                      ((0, 6), (1, 7))]
-  for position in positions:
-    check canAttack(position.white, position.black)
+  # Test the ability of one queen to attack another
+  test "true attacks":
+    var positions: seq[tuple[white, black: tuple[rank, file: int]]] = @[((2, 3), (5, 6)),
+                                                                        ((2, 6), (5, 3)),
+                                                                        ((2, 4), (2, 7)),
+                                                                        ((5, 4), (2, 4)),
+                                                                        ((1, 1), (6, 6)),
+                                                                        ((0, 6), (1, 7))]
+    for position in positions:
+      check canAttack(position.white, position.black)
 
-test "false attacks":
-  var positions: seq[tuple[white, black: tuple[rank, file: int]]] = @[((4, 2), (0, 5)),
-                                                                      ((2, 3), (4, 7))]
-  for position in positions:
-    check (not canAttack(position.white, position.black))
-
-
-# Bonus: graphical board representation
-test "board1":
-  check board((2, 3), (5, 6)) == @["________",
-                                   "________",
-                                   "___W____",
-                                   "________",
-                                   "________",
-                                   "______B_",
-                                   "________",
-                                   "________"]
-
-test "board2":
-  check board((0, 6), (1, 7)) == @["______W_",
-                                   "_______B",
-                                   "________",
-                                   "________",
-                                   "________",
-                                   "________",
-                                   "________",
-                                   "________"]
+  test "false attacks":
+    var positions: seq[tuple[white, black: tuple[rank, file: int]]] = @[((4, 2), (0, 5)),
+                                                                        ((2, 3), (4, 7))]
+    for position in positions:
+      check (not canAttack(position.white, position.black))
 
 
-# Bonus: raise an error for invalid positions
-test "invalid position on board":
-  expect(ValueError):
-    discard board((0, 0), (7, 8))
+  # Bonus: graphical board representation
+  test "board1":
+    check board((2, 3), (5, 6)) == @["________",
+                                     "________",
+                                     "___W____",
+                                     "________",
+                                     "________",
+                                     "______B_",
+                                     "________",
+                                     "________"]
+
+  test "board2":
+    check board((0, 6), (1, 7)) == @["______W_",
+                                     "_______B",
+                                     "________",
+                                     "________",
+                                     "________",
+                                     "________",
+                                     "________",
+                                     "________"]
 
 
-# Bonus: raise an error if queens occupy the same position
-test "queens in same position in canAttack":
-  expect(ValueError):
-    discard canAttack((2, 2), (2, 2))
+  # Bonus: raise an error for invalid positions
+  test "invalid position on board":
+    expect(ValueError):
+      discard board((0, 0), (7, 8))
 
-test "queens in same position on board":
-  expect(ValueError):
-    discard board((2, 2), (2, 2))
+
+  # Bonus: raise an error if queens occupy the same position
+  test "queens in same position in canAttack":
+    expect(ValueError):
+      discard canAttack((2, 2), (2, 2))
+
+  test "queens in same position on board":
+    expect(ValueError):
+      discard board((2, 2), (2, 2))
