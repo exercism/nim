6244e0256c0259ede5e19554f041e3f62fa8d692
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:01 +0100

    acronym: Update tests to 1.7.0

--- exercises/acronym/acronym_test.nim
+++ exercises/acronym/acronym_test.nim
@@ -1,20 +1,33 @@
 import unittest
-
 import acronym
 
+# version 1.7.0
+
 suite "Acronym":
-  
   test "basic":
     check abbreviate("Portable Network Graphics") == "PNG"
 
-  test "lowercase_words":
+  test "lowercase words":
     check abbreviate("Ruby on Rails") == "ROR"
 
   test "punctuation":
     check abbreviate("First In, First Out") == "FIFO"
-    
-  test "all_caps_words":
+
+  test "all caps word":
     check abbreviate("GNU Image Manipulation Program") == "GIMP"
 
-  test "punctuation_without_whitespace":
+  test "punctuation without whitespace":
     check abbreviate("Complementary metal-oxide semiconductor") == "CMOS"
+
+  test "very long abbreviation":
+    check abbreviate("Rolling On The Floor Laughing So Hard That " &
+                     "My Dogs Came Over And Licked Me") == "ROTFLSHTMDCOALM"
+
+  test "consecutive delimiters":
+    check abbreviate("Something - I made up from thin air") == "SIMUFTA"
+
+  test "apostrophes":
+    check abbreviate("Halley's Comet") == "HC"
+
+  test "underscore emphasis":
+    check abbreviate("The Road _Not_ Taken") == "TRNT"
