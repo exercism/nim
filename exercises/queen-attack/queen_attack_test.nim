ee53c160646ee8905f2279bf25d3d89a338a6587
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:21 +0100

    queen-attack: Remove duplicate tests

--- exercises/queen-attack/queen_attack_test.nim
+++ exercises/queen-attack/queen_attack_test.nim
@@ -50,26 +50,6 @@ test "invalid position in canAttack":
   expect(ValueError):
     discard canAttack((0, 0), (7, 8))
 
-test "true attacks":
-  var positions: seq[tuple[white, black: tuple[rank, file: int]]] = @[((2, 3), (5, 6)),
-                                                                      ((2, 6), (5, 3)),
-                                                                      ((2, 4), (2, 7)),
-                                                                      ((5, 4), (2, 4)),
-                                                                      ((1, 1), (6, 6)),
-                                                                      ((0, 6), (1, 7))]
-  for position in positions:
-    check canAttack(position.white, position.black)
-
-test "false attacks":
-  var positions: seq[tuple[white, black: tuple[rank, file: int]]] = @[((4, 2), (0, 5)),
-                                                                      ((2, 3), (4, 7))]
-  for position in positions:
-    check (not canAttack(position.white, position.black))
-
-test "invalid position on board":
-  expect(ValueError):
-    discard board((0, 0), (7, 8))
-
 test "queens in same position on board":
   expect(ValueError):
     discard board((2, 2), (2, 2))
