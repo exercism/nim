e81f3f0d33baeb8efef92a54ad3ad77bd1f2407e
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:11 +0100

    gigasecond: Update tests to 2.0.0

--- exercises/gigasecond/gigasecond_test.nim
+++ exercises/gigasecond/gigasecond_test.nim
@@ -1,32 +1,30 @@
 import unittest, times
-
 import gigasecond
 
-suite "Gigasecond":
+# version 2.0.0
 
+suite "Gigasecond":
   test "date only specification of time":
-    let birthday = initDateTime(25, mApr, 2011, 0, 0, 0, utc())
+    let moment   = initDateTime(25, mApr, 2011, 0, 0, 0, utc())
     let expected = initDateTime(1, mJan, 2043, 1, 46, 40, utc())
-    check addGigasecond(birthday) == expected
+    check addGigasecond(moment) == expected
 
   test "second test for date only specification of time":
-    let birthday = initDateTime(13, mJun, 1977, 0, 0, 0, utc())
+    let moment   = initDateTime(13, mJun, 1977, 0, 0, 0, utc())
     let expected = initDateTime(19, mFeb, 2009, 1, 46, 40, utc())
-    check addGigasecond(birthday) == expected
+    check addGigasecond(moment) == expected
 
   test "third test for date only specification of time":
-    let birthday = initDateTime(19, mJul, 1959, 0, 0, 0, utc())
+    let moment   = initDateTime(19, mJul, 1959, 0, 0, 0, utc())
     let expected = initDateTime(27, mMar, 1991, 1, 46, 40, utc())
-    check addGigasecond(birthday) == expected
+    check addGigasecond(moment) == expected
 
   test "full time specified":
-    let birthday = initDateTime(24, mJan, 2015, 22, 0, 0, utc())
+    let moment   = initDateTime(24, mJan, 2015, 22, 0, 0, utc())
     let expected = initDateTime(2, mOct, 2046, 23, 46, 40, utc())
-    check addGigasecond(birthday) == expected
+    check addGigasecond(moment) == expected
 
   test "full time with day roll-over":
-    let birthday = initDateTime(24, mJan, 2015, 23, 59, 59, utc())
+    let moment   = initDateTime(24, mJan, 2015, 23, 59, 59, utc())
     let expected = initDateTime(3, mOct, 2046, 1, 46, 39, utc())
-    check addGigasecond(birthday) == expected
-
-
+    check addGigasecond(moment) == expected
