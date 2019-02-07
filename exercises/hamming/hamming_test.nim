8d1ac56b744b933a7290208bd47e4cb7fdd48ceb
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:13 +0100

    hamming: Update tests to 2.3.0

--- exercises/hamming/hamming_test.nim
+++ exercises/hamming/hamming_test.nim
@@ -1,46 +1,22 @@
 import unittest
-
 import hamming
 
-suite "Hamming":
+# version 2.3.0
 
+suite "Hamming":
   test "empty strands":
     check distance("", "") == 0
-  
-  test "identical strands":
+
+  test "single letter identical strands":
     check distance("A", "A") == 0
 
+  test "single letter different strands":
+    check distance("G", "T") == 1
+
   test "long identical strands":
-    check distance("GGACTGA", "GGACTGA") == 0
+    check distance("GGACTGAAATCTG", "GGACTGAAATCTG") == 0
 
-  test "complete distance in single nucleotide strands":
-    check distance("A", "G") == 1
-
-  test "complete distance in small strands":
-    check distance("AG", "CT") == 2
-
-  test "small distance in small strands":
-    check distance("AT", "CT") == 1
-
-  test "small distance":
-    check distance("GGACG", "GGTCG") == 1
-
-  test "small distance in long strands":
-    check distance("ACCAGGG", "ACTATGG") == 2
-
-  test "non-unique character in first strand":
-    check distance("AAG", "AAA") == 1
-
-  test "non-unique character in second strand":
-    check distance("AAA", "AAG") == 1
-
-  test "same nucleotides in different positions":
-    check distance("TAG", "GAT") == 2
-
-  test "large distance":
-    check distance("GATACA", "GCATAA") == 4
-
-  test "large distance in off-by-one strand":
+  test "long different strands":
     check distance("GGACGGATTCTG", "AGGACGGATTCT") == 9
 
   test "disallow first strand longer":
@@ -50,3 +26,11 @@ suite "Hamming":
   test "disallow second strand longer":
     expect(ValueError):
       discard distance("ATA", "AGTG")
+
+  test "disallow left empty strand":
+    expect(ValueError):
+      discard distance("", "G")
+
+  test "disallow right empty strand":
+    expect(ValueError):
+      discard distance("G", "")
