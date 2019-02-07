18032e2966c57753ad761f7db64820b27b1da9b4
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:03 +0100

    anagram: Update tests to 1.4.0

--- exercises/anagram/anagram_test.nim
+++ exercises/anagram/anagram_test.nim
@@ -1,8 +1,9 @@
 import unittest
-from anagram import detectAnagrams
+import anagram
+
+# version 1.4.0
 
 suite "Anagram":
-  
   test "no matches":
     let
       word = "diaper"
@@ -11,15 +12,7 @@ suite "Anagram":
       detected = detectAnagrams(word, candidates)
     check detected == expected
 
-  test "detect simple anagram":
-    let
-      word = "ant"
-      candidates = @["tan", "stand", "at"]
-      expected = @["tan"]
-      detected = detectAnagrams(word, candidates)
-    check detected == expected
-
-  test "detect multiple anagrams":
+  test "detects two anagrams":
     let
       word = "master"
       candidates = @["stream", "pigeon", "maters"]
@@ -27,15 +20,7 @@ suite "Anagram":
       detected = detectAnagrams(word, candidates)
     check detected == expected
 
-  test "does not confuse different duplicate characters":
-    let
-      word = "galea"
-      candidates = @["eagle"]
-      expected: seq[string] = @[]
-      detected = detectAnagrams(word, candidates)
-    check detected == expected
-
-  test "eliminate anagram subsets":
+  test "does not detect anagram subsets":
     let
       word = "good"
       candidates = @["dog", "goody"]
@@ -43,7 +28,7 @@ suite "Anagram":
       detected = detectAnagrams(word, candidates)
     check detected == expected
 
-  test "detect anagram":
+  test "detects anagram":
     let
       word = "listen"
       candidates = @["enlists", "google", "inlets", "banana"]
@@ -51,15 +36,24 @@ suite "Anagram":
       detected = detectAnagrams(word, candidates)
     check detected == expected
 
-  test "multiple anagrams":
+  test "detects three anagrams":
     let
       word = "allergy"
-      candidates = @["gallery", "ballerina", "regally", "clergy", "largely", "leading"]
+      candidates = @["gallery", "ballerina", "regally",
+                     "clergy", "largely", "leading"]
       expected = @["gallery", "regally", "largely"]
       detected = detectAnagrams(word, candidates)
     check detected == expected
 
-  test "anagrams are case insensitive":
+  test "does not detect non-anagrams with identical checksum":
+    let
+      word = "mass"
+      candidates = @["last"]
+      expected: seq[string] = @[]
+      detected = detectAnagrams(word, candidates)
+    check detected == expected
+
+  test "detects anagrams case-insensitively":
     let
       word = "Orchestra"
       candidates = @["cashregister", "Carthorse", "radishes"]
@@ -67,10 +61,42 @@ suite "Anagram":
       detected = detectAnagrams(word, candidates)
     check detected == expected
 
-  test "same word isn't anagram":
+  test "detects anagrams using case-insensitive subject":
+    let
+      word = "Orchestra"
+      candidates = @["cashregister", "carthorse", "radishes"]
+      expected = @["carthorse"]
+      detected = detectAnagrams(word, candidates)
+    check detected == expected
+
+  test "detects anagrams using case-insensitive possible matches":
+    let
+      word = "orchestra"
+      candidates = @["cashregister", "Carthorse", "radishes"]
+      expected = @["Carthorse"]
+      detected = detectAnagrams(word, candidates)
+    check detected == expected
+
+  test "does not detect a anagram if the original word is repeated":
     let
       word = "go"
-      candidates = @["go", "Go", "GO"]
+      candidates = @["go Go GO"]
+      expected: seq[string] = @[]
+      detected = detectAnagrams(word, candidates)
+    check detected == expected
+
+  test "anagrams must use all letters exactly once":
+    let
+      word = "tapper"
+      candidates = @["patter"]
+      expected: seq[string] = @[]
+      detected = detectAnagrams(word, candidates)
+    check detected == expected
+
+  test "words are not anagrams of themselves (case-insensitive)":
+    let
+      word = "BANANA"
+      candidates = @["BANANA", "Banana", "banana"]
       expected: seq[string] = @[]
       detected = detectAnagrams(word, candidates)
     check detected == expected
