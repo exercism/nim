5b51bcd96e597d2f8528d232eb572682f50ca362
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:12 +0100

    grade-school: Declare compliance with 1.0.0

--- exercises/grade-school/grade_school_test.nim
+++ exercises/grade-school/grade_school_test.nim
@@ -1,35 +1,30 @@
 import unittest
+import grade_school
+
+# version 1.0.0
 
 # Given students' names along with the grade that they are in,
 # create a roster for the school.
 
-import grade_school
-
 suite "Grade School":
-
-  test "Adding a student adds them to the sorted roster":
+  test "adding a student adds them to the sorted roster":
     let school = School(students: @[("Aimee", 2)])
-
     check school.roster() == @["Aimee"]
 
-  test "Adding more student adds them to the sorted roster":
+  test "adding more students adds them to the sorted roster":
     let school = School(students: @[("Blair", 2), ("James", 2), ("Paul", 2)])
-
     check school.roster() == @["Blair", "James", "Paul"]
 
-  test "Adding students to different grades adds them to the same sorted roster":
+  test "adding students to different grades adds them to the same sorted roster":
     let school = School(students: @[("Chelsea", 3), ("Logan", 7)])
-    
     check school.roster() == @["Chelsea", "Logan"]
 
-  test "Roster returns an empty list if there are no students enrolled":
+  test "roster returns an empty list if there are no students enrolled":
     let school = School(students: @[])
     let expected: seq[string] = @[]
-
     check school.roster() == expected
 
-
-  test "Student names with grades are displayed in the same sorted roster":
+  test "roster is sorted by grade then name":
     let school = School(students: @[
       ("Peter", 2),
       ("Anna", 1),
@@ -39,16 +34,13 @@ suite "Grade School":
       ("Jim", 3),
       ("Charlie", 1)
     ])
-  
     check school.roster() == @["Anna", "Barb", "Charlie", "Alex", "Peter", "Zoe", "Jim"]
 
-  test "Grade returns the students in that grade in alphabetical order":
+  test "grade returns the students in that grade in alphabetical order":
     let school = School(students: @[("Franklin", 5), ("Bradley", 5), ("Jeff", 1)])
-  
     check school.grade(5) == @["Bradley", "Franklin"]
 
-  test "Grade returns an empty list if there are no students in that grade":
+  test "grade returns an empty list if there are no students in that grade":
     let school = School(students: @[])
     let expected: seq[string] = @[]
-
     check school.grade(1) == expected
