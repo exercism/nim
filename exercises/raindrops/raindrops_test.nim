82ebd95ffb506d6dfcc0230c8937ff8140210cd3
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:25 +0100

    raindrops: Update tests to 1.1.0

--- exercises/raindrops/raindrops_test.nim
+++ exercises/raindrops/raindrops_test.nim
@@ -1,53 +1,59 @@
 import unittest
-
 import raindrops
 
+# version 1.1.0
+
 suite "Raindrops":
-  
-  test "convert 1":
+  test "the sound for 1 is 1":
     check convert(1) == "1"
 
-  test "convert 3":
+  test "the sound for 3 is Pling":
     check convert(3) == "Pling"
 
-  test "convert 5":
+  test "the sound for 5 is Plang":
     check convert(5) == "Plang"
 
-  test "convert 7":
+  test "the sound for 7 is Plong":
     check convert(7) == "Plong"
 
-  test "convert 6":
+  test "the sound for 6 is Pling as it has a factor 3":
     check convert(6) == "Pling"
 
-  test "convert 9":
+  test "the sound for 8 is 8":
+    check convert(8) == "8"
+
+  test "the sound for 9 is Pling as it has a factor 3":
     check convert(9) == "Pling"
 
-  test "convert 10":
+  test "the sound for 10 is Plang as it has a factor 5":
     check convert(10) == "Plang"
 
-  test "convert 14":
+  test "the sound for 14 is Plong as it has a factor 7":
     check convert(14) == "Plong"
 
-  test "convert 15":
+  test "the sound for 15 is PlingPlang as it has factors 3 and 5":
     check convert(15) == "PlingPlang"
 
-  test "convert 21":
+  test "the sound for 21 is PlingPlong as it has factors 3 and 7":
     check convert(21) == "PlingPlong"
 
-  test "convert 25":
+  test "the sound for 25 is Plang as it has a factor 5":
     check convert(25) == "Plang"
 
-  test "convert 35":
+  test "the sound for 27 is Pling as it has a factor 3":
+    check convert(27) == "Pling"
+
+  test "the sound for 35 is PlangPlong as it has factors 5 and 7":
     check convert(35) == "PlangPlong"
 
-  test "convert 49":
+  test "the sound for 49 is Plong as it has a factor 7":
     check convert(49) == "Plong"
 
-  test "convert 52":
+  test "the sound for 52 is 52":
     check convert(52) == "52"
 
-  test "convert 105":
+  test "the sound for 105 is PlingPlangPlong as it has factors 3, 5 and 7":
     check convert(105) == "PlingPlangPlong"
 
-  test "convert 12121":
-    check convert(12121) == "12121"
+  test "the sound for 3125 is Plang as it has a factor 5":
+    check convert(3125) == "Plang"
