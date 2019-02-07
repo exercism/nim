cd41f702d59c3d9d1b9f79fc252020859cb08aca
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:10 +0100

    difference-of-squares: Update tests to 1.2.0

--- exercises/difference-of-squares/difference_of_squares_test.nim
+++ exercises/difference-of-squares/difference_of_squares_test.nim
@@ -1,22 +1,32 @@
 import unittest
 import difference_of_squares
 
-suite "Difference of Squares":
+# version 1.2.0
 
-  test "square of sum, 5":
+suite "Difference of Squares":
+  test "square of sum 1":
+    check squareOfSum(1) == 1
+
+  test "square of sum 5":
     check squareOfSum(5) == 225
 
-  test "sum of squares, 5":
+  test "square of sum 100":
+    check squareOfSum(100) == 25_502_500
+
+  test "sum of squares 1":
+    check sumOfSquares(1) == 1
+
+  test "sum of squares 5":
     check sumOfSquares(5) == 55
 
-  test "difference, 5":
+  test "sum of squares 100":
+    check sumOfSquares(100) == 338_350
+
+  test "difference of squares 1":
+    check difference(1) == 0
+
+  test "difference of squares 5":
     check difference(5) == 170
 
-  test "square of sum, 100":
-    check squareOfSum(100) == 25502500
-
-  test "sum of squares, 100":
-    check sumOfSquares(100) == 338350
-
-  test "difference, 100":
-    check difference(100) == 25164150
+  test "difference of squares 100":
+    check difference(100) == 25_164_150
