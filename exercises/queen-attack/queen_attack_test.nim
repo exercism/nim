305c8a03e4faa71c907bdfa85235b17b2dcb944f
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:22 +0100

    queen-attack: Rearrange tests, and indicate bonus tests

--- exercises/queen-attack/queen_attack_test.nim
+++ exercises/queen-attack/queen_attack_test.nim
@@ -2,30 +2,13 @@ import unittest
 
 import queen_attack
 
-const answer1 = @["________",
-                  "________",
-                  "___W____",
-                  "________",
-                  "________",
-                  "______B_",
-                  "________",
-                  "________"]
+# Raise an error for invalid positions
+test "invalid position in canAttack":
+  expect(ValueError):
+    discard canAttack((0, 0), (7, 8))
 
-test "board1":
-  check board((2, 3), (5, 6)) == answer1
 
-const answer2 = @["______W_",
-                  "_______B",
-                  "________",
-                  "________",
-                  "________",
-                  "________",
-                  "________",
-                  "________"]
-
-test "board2":
-  check board((0, 6), (1, 7)) == answer2
-
+# Test the ability of one queen to attack another
 test "true attacks":
   var positions: seq[tuple[white, black: tuple[rank, file: int]]] = @[((2, 3), (5, 6)),
                                                                       ((2, 6), (5, 3)),
@@ -42,18 +25,40 @@ test "false attacks":
   for position in positions:
     check (not canAttack(position.white, position.black))
 
+
+# Bonus: graphical board representation
+test "board1":
+  check board((2, 3), (5, 6)) == @["________",
+                                   "________",
+                                   "___W____",
+                                   "________",
+                                   "________",
+                                   "______B_",
+                                   "________",
+                                   "________"]
+
+test "board2":
+  check board((0, 6), (1, 7)) == @["______W_",
+                                   "_______B",
+                                   "________",
+                                   "________",
+                                   "________",
+                                   "________",
+                                   "________",
+                                   "________"]
+
+
+# Bonus: raise an error for invalid positions
 test "invalid position on board":
   expect(ValueError):
     discard board((0, 0), (7, 8))
 
-test "invalid position in canAttack":
+
+# Bonus: raise an error if queens occupy the same position
+test "queens in same position in canAttack":
   expect(ValueError):
-    discard canAttack((0, 0), (7, 8))
+    discard canAttack((2, 2), (2, 2))
 
 test "queens in same position on board":
   expect(ValueError):
     discard board((2, 2), (2, 2))
-
-test "queens in same position in canAttack":
-  expect(ValueError):
-    discard canAttack((2, 2), (2, 2))
