4b690ffbed46bbb7386ee803b6d2b1dcc8a4645b
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-07 20:00:34 +0100

    sum-of-multiples: Update tests and example to 1.5.0

--- exercises/sum-of-multiples/example.nim
+++ exercises/sum-of-multiples/example.nim
@@ -2,5 +2,5 @@ import sequtils
 
 proc sum*(limit: int, factors: seq[int]): int =
   for num in 0..<limit:
-    if anyIt(factors, num mod it == 0):
+    if factors.filterIt(it != 0).anyIt(num mod it == 0):
       result = result + num
--- exercises/sum-of-multiples/sum_of_multiples_test.nim
+++ exercises/sum-of-multiples/sum_of_multiples_test.nim
@@ -1,44 +1,53 @@
 import unittest
-
 import sum_of_multiples
 
-suite "Sum of Multiples":
+# version 1.5.0
 
-  test "multiples of 3 or 5 up to 1":
+suite "Sum of Multiples":
+  test "no multiples within limit":
     check sum(1, @[3, 5]) == 0
 
-  test "multiples of 3 or 5 up to 4":
+  test "one factor has multiples within limit":
     check sum(4, @[3, 5]) == 3
 
-  test "multiples of 3 up to 7":
+  test "more than one multiple within limit":
     check sum(7, @[3]) == 9
 
-  test "multiples of 3 or 5 up to 10":
+  test "more than one factor with multiples within limit":
     check sum(10, @[3, 5]) == 23
 
-  test "multiples of 3 or 5 up to 100":
+  test "each multiple is only counted once":
     check sum(100, @[3, 5]) == 2318
 
-  test "multiples of 3 or 5 up to 1000":
-    check sum(1000, @[3, 5]) == 233168
+  test "a much larger limit":
+    check sum(1000, @[3, 5]) == 233_168
 
-  test "multiples of 7, 13 or 17 up to 20":
+  test "three factors":
     check sum(20, @[7, 13, 17]) == 51
 
-  test "multiples of 4 or 6 up to 15":
+  test "factors not relatively prime":
     check sum(15, @[4, 6]) == 30
 
-  test "multiples of 5, 6 or 8 up to 150":
+  test "some pairs of factors relatively prime and some not":
     check sum(150, @[5, 6, 8]) == 4419
 
-  test "multiples of 5 or 25 up to 51":
+  test "one factor is a multiple of another":
     check sum(51, @[5, 25]) == 275
 
-  test "multiples of 43 or 47 up to 10000":
-    check sum(10000, @[43, 47]) == 2203160
+  test "much larger factors":
+    check sum(10_000, @[43, 47]) == 2_203_160
 
-  test "multiples of 1 up to 100":
+  test "all numbers are multiples of 1":
     check sum(100, @[1]) == 4950
 
-  test "multiples of 1 up to 100":
-    check sum(10000, @[]) == 0
+  test "no factors means an empty sum":
+    check sum(10_000, @[]) == 0
+
+  test "the only multiple of 0 is 0":
+    check sum(1, @[0]) == 0
+
+  test "the factor 0 does not affect the sum of multiples of other factors":
+    check sum(4, @[3, 0]) == 3
+
+  test "solutions using include-exclude must extend to cardinality greater than 3":
+    check sum(10_000, @[2, 3, 5, 7, 11]) == 39_614_537
