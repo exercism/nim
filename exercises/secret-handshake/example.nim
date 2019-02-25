40ef5ab901fe753ff79e93876cabcb3d05d4c443
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-25 19:00:01 +0100

    secret-handshake: Reformat example
    Add missing final newline to test file

--- exercises/secret-handshake/example.nim
+++ exercises/secret-handshake/example.nim
@@ -1,23 +1,22 @@
-import 
-    sequtils, algorithm
+import algorithm
 
 const
-    signals = [
-      "wink", 
-      "double blink", 
-      "close your eyes", 
-      "jump"
-    ]
-    reverse_bit = 4
+  signals = [
+    "wink",
+    "double blink",
+    "close your eyes",
+    "jump"
+  ]
+  reverseBit = 4
 
 
 proc bitset(input: Natural, position: Natural): bool =
-    (input and 1 shl position) != 0
+  (input and 1 shl position) != 0
 
 proc commands*(input: Natural): seq[string] =
-    for index, item in signals.pairs:
-      if input.bitset(index):
-        result.add(item)
+  for index, item in signals:
+    if input.bitset(index):
+      result.add(item)
 
-    if input.bitset(reverse_bit):
-      return result.reversed
+  if input.bitset(reverseBit):
+    result.reverse
