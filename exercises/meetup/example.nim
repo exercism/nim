37e978bfc689d634128a31b3ceea95d92002e188
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-25 20:00:09 +0100

    Implement exercise: meetup (#168)

---
+++ exercises/meetup/example.nim
@@ -0,0 +1,14 @@
+import strformat, times
+
+type Schedule* = enum
+  Teenth, First, Second, Third, Fourth, Last
+const startDay: array[Schedule, int] = [13, 1, 8, 15, 22, 1]
+
+proc meetup*(y: int, m: int, sched: Schedule, d: Weekday): string =
+  var dt = parse(&"{y}-{m}-{startDay[sched]}-12", "yyyy-M-d-hh", utc())
+
+  if sched == Last:
+    dt = dt + 1.months - 7.days
+
+  dt = dt + ((d.ord - dt.weekday.ord + 7) mod 7).days
+  result = dt.format("yyyy-MM-dd")
