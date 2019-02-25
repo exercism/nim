423290a696d46a8a5802b95d9bed97e2021f4a48
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-25 20:00:11 +0100

    Implement exercise: perfect-numbers (#170)

---
+++ exercises/perfect-numbers/perfect_numbers_test.nim
@@ -0,0 +1,46 @@
+import unittest
+import perfect_numbers
+
+# version 1.1.0
+
+suite "Perfect Numbers":
+  test "smallest perfect number is classified correctly":
+    check classify(6) == Perfect
+
+  test "medium perfect number is classified correctly":
+    check classify(28) == Perfect
+
+  test "large perfect number is classified correctly":
+    check classify(33_550_336) == Perfect
+
+  test "smallest abundant number is classified correctly":
+    check classify(12) == Abundant
+
+  test "medium abundant number is classified correctly":
+    check classify(30) == Abundant
+
+  test "large abundant number is classified correctly":
+    check classify(33_550_335) == Abundant
+
+  test "smallest prime deficient number is classified correctly":
+    check classify(2) == Deficient
+
+  test "smallest non-prime deficient number is classified correctly":
+    check classify(4) == Deficient
+
+  test "medium deficient number is classified correctly":
+    check classify(32) == Deficient
+
+  test "large deficient number is classified correctly":
+    check classify(33_550_337) == Deficient
+
+  test "edge case (no factors other than itself) is classified correctly":
+    check classify(1) == Deficient
+
+  test "zero is rejected (not a positive number)":
+    expect ValueError:
+      discard classify(0)
+
+  test "negative integer is rejected (not a positive number)":
+    expect ValueError:
+      discard classify(-1)
