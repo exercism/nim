c7ded35e11fdb9792a7533c5b55069ba1f9ee3bc
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-25 20:00:12 +0100

    Implement exercise: prime-factors (#171)

---
+++ exercises/prime-factors/prime_factors_test.nim
@@ -0,0 +1,26 @@
+import unittest
+import prime_factors
+
+# version 1.1.0
+
+suite "Prime Factors":
+  test "no factors":
+    check primeFactors(1).len == 0
+
+  test "prime number":
+    check primeFactors(2) == @[2]
+
+  test "square of a prime":
+    check primeFactors(9) == @[3, 3]
+
+  test "cube of a prime":
+    check primeFactors(8) == @[2, 2, 2]
+
+  test "product of primes and non-primes":
+    check primeFactors(12) == @[2, 2, 3]
+
+  test "product of primes":
+    check primeFactors(901_255) == @[5, 17, 23, 461]
+
+  test "factors include a large prime":
+    check primeFactors(93_819_012_551) == @[11, 9_539, 894_119]
