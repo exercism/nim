1f3abc7064d8912f43fd9a6b86c5be84856001b3
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-25 20:00:11 +0100

    Implement exercise: perfect-numbers (#170)

---
+++ exercises/perfect-numbers/example.nim
@@ -0,0 +1,21 @@
+import math
+
+type Classification* = enum
+  Perfect, Deficient, Abundant
+
+func aliquotSum(n: int): int =
+  let intSqrt = n.float.sqrt.int
+  for x in 1 .. intSqrt:
+    if n mod x == 0:
+      result += x + n div x
+  result -= n
+  if intSqrt * intSqrt == n:
+    result -= intSqrt
+
+func classify*(n: int): Classification =
+  if n <= 0:
+    raise newException(ValueError, "Not a positive number: " & $n)
+  let sum = aliquotSum(n)
+  if sum == n: Perfect
+  elif sum < n: Deficient
+  else: Abundant
