05bda7e71d1b4acc77634b96c01819e26a46d5c8
ee7 <45465154+ee7@users.noreply.github.com>
2019-02-25 20:00:03 +0100

    Implement exercise: high-scores (#162)

---
+++ exercises/high-scores/example.nim
@@ -0,0 +1,10 @@
+import algorithm
+
+func latest*(scores: openArray[int]): int =
+  scores[^1]
+
+func personalBest*(scores: openArray[int]): int =
+  scores.max
+
+func personalTopThree*(scores: openArray[int]): seq[int] =
+  scores.sorted(cmp = system.cmp, order = Descending)[0 .. min(2, scores.high)]
---
+++ exercises/high-scores/high_scores_test.nim
@@ -0,0 +1,27 @@
+import unittest
+import high_scores
+
+# version 4.0.0
+
+suite "High Scores":
+  test "latest score":
+    check latest(@[100, 0, 90, 30]) == 30
+
+  test "personal best":
+    check personalBest(@[40, 100, 70]) == 100
+
+  test "personal top three from a list of scores":
+    check personalTopThree(@[10, 30, 90, 30, 100, 20,
+                             10, 0, 30, 40, 40, 70, 70]) == @[100, 90, 70]
+
+  test "personal top highest to lowest":
+    check personalTopThree(@[20, 10, 30]) == @[30, 20, 10]
+
+  test "personal top when there is a tie":
+    check personalTopThree(@[40, 20, 40, 30]) == @[40, 40, 30]
+
+  test "personal top when there are less than 3":
+    check personalTopThree(@[30, 70]) == @[70, 30]
+
+  test "personal top when there is only one":
+    check personalTopThree(@[40]) == @[40]
