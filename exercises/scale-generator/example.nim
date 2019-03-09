f395e196566ad6d55da896c2209f71cf12ee3231
ee7 <45465154+ee7@users.noreply.github.com>
2019-03-09 20:00:10 +0100

    Implement exercise: scale-generator (#191)

---
+++ exercises/scale-generator/example.nim
@@ -0,0 +1,16 @@
+import sets, strutils, tables
+
+const
+  chromaticSharps = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+  chromaticFlats = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
+  flatKeys = ["F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"].toSet
+  semitones = {'m': 1, 'M': 2, 'A': 3}.toTable
+  defaultIntervals = "m".repeat(12)
+
+func scale*(tonic: string, intervals = defaultIntervals): seq[string] =
+  let chromatic = if tonic in flatKeys: chromaticFlats else: chromaticSharps
+  var i = chromatic.find(tonic.capitalizeAscii)
+
+  for c in intervals:
+    result &= chromatic[i mod 12]
+    i += semitones[c]
---
+++ exercises/scale-generator/scale_generator_test.nim
@@ -0,0 +1,58 @@
+import unittest
+import scale_generator
+
+# version 2.0.0
+
+suite "Scale Generator":
+  test "chromatic scale with sharps":
+    check scale("C") == @["C", "C#", "D", "D#", "E", "F",
+                          "F#", "G", "G#", "A", "A#", "B"]
+
+  test "chromatic scale with flats":
+    check scale("F") == @["F", "Gb", "G", "Ab", "A", "Bb",
+                          "B", "C", "Db", "D", "Eb", "E"]
+
+  test "simple major scale":
+    check scale("C", "MMmMMMm") == @["C", "D", "E", "F", "G", "A", "B"]
+
+  test "major scale with sharps":
+    check scale("G", "MMmMMMm") == @["G", "A", "B", "C", "D", "E", "F#"]
+
+  test "major scale with flats":
+    check scale("F", "MMmMMMm") == @["F", "G", "A", "Bb", "C", "D", "E"]
+
+  test "minor scale with sharps":
+    check scale("f#", "MmMMmMM") == @["F#", "G#", "A", "B", "C#", "D", "E"]
+
+  test "minor scale with flats":
+    check scale("bb", "MmMMmMM") == @["Bb", "C", "Db", "Eb", "F", "Gb", "Ab"]
+
+  test "dorian mode":
+    check scale("d", "MmMMMmM") == @["D", "E", "F", "G", "A", "B", "C"]
+
+  test "mixolydian mode":
+    check scale("Eb", "MMmMMmM") == @["Eb", "F", "G", "Ab", "Bb", "C", "Db"]
+
+  test "lydian mode":
+    check scale("a", "MMMmMMm") == @["A", "B", "C#", "D#", "E", "F#", "G#"]
+
+  test "phrygian mode":
+    check scale("e", "mMMMmMM") == @["E", "F", "G", "A", "B", "C", "D"]
+
+  test "locrian mode":
+    check scale("g", "mMMmMMM") == @["G", "Ab", "Bb", "C", "Db", "Eb", "F"]
+
+  test "harmonic minor":
+    check scale("d", "MmMMmAm") == @["D", "E", "F", "G", "A", "Bb", "Db"]
+
+  test "octatonic":
+    check scale("C", "MmMmMmMm") == @["C", "D", "D#", "F", "F#", "G#", "A", "B"]
+
+  test "hexatonic":
+    check scale("Db", "MMMMMM") == @["Db", "Eb", "F", "G", "A", "B"]
+
+  test "pentatonic":
+    check scale("A", "MMAMA") == @["A", "B", "C#", "E", "F#"]
+
+  test "enigmatic":
+    check scale("G", "mAMMMmm") == @["G", "G#", "B", "C#", "D#", "F", "F#"]
