8f951472d18dc2b34138b382747b6ae17fa21c54
ee7 <45465154+ee7@users.noreply.github.com>
2019-03-09 20:00:11 +0100

    Implement exercise: twelve-days (#192)

---
+++ exercises/twelve-days/example.nim
@@ -0,0 +1,48 @@
+import strformat
+
+type
+  DayRange = range[1..12]
+  DayArray[T] = array[DayRange, T]
+
+const gifts: DayArray[string] = [
+  "a Partridge in a Pear Tree",
+  "two Turtle Doves",
+  "three French Hens",
+  "four Calling Birds",
+  "five Gold Rings",
+  "six Geese-a-Laying",
+  "seven Swans-a-Swimming",
+  "eight Maids-a-Milking",
+  "nine Ladies Dancing",
+  "ten Lords-a-Leaping",
+  "eleven Pipers Piping",
+  "twelve Drummers Drumming"]
+
+const nth: DayArray[string] = [
+  "first", "second", "third", "fourth", "fifth", "sixth",
+  "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+
+func generateVerses: tuple[song: string, indices: DayArray[int]] =
+  ## Returns a tuple of:
+  ##   `song`: the song text, including two newlines after every verse.
+  ##   `indices`: the index in `song` of the first character for each verse.
+  for n in nth.low .. nth.high:
+    result.indices[n] = result.song.len
+    result.song &= &"On the {nth[n]} day of Christmas my true love gave to me: "
+    for i in countdown(n, 1):
+      case i
+      of 3..12:
+        result.song &= &"{gifts[i]}, "
+      of 2:
+        result.song &= &"{gifts[i]}, and "
+      of 1:
+        result.song &= &"{gifts[i]}.\n\n"
+
+const pair = generateVerses() # Generate the lyrics at compile-time.
+const song = pair[0] # `const (song, indices) = generateVerses()` in Nim 0.19.9
+const indices = pair[1]
+
+func recite*(start: DayRange, stop = start): string =
+  let firstChar = indices[start]
+  let lastChar = if stop == 12: song.high - 2 else: indices[stop + 1] - 3
+  result = song[firstChar .. lastChar] # Excludes the final two newlines.
---
+++ exercises/twelve-days/twelve_days_test.nim
@@ -0,0 +1,82 @@
+import unittest
+import twelve_days
+
+# version 1.2.0
+
+suite "Twelve Days":
+  # Single verse: zero newline characters at the end.
+  test "first day a partridge in a pear tree":
+    const expected = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+    check recite(1) == expected
+
+  test "second day two turtle doves":
+    const expected = "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(2) == expected
+
+  test "third day three french hens":
+    const expected = "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(3) == expected
+
+  test "fourth day four calling birds":
+    const expected = "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(4) == expected
+
+  test "fifth day five gold rings":
+    const expected = "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(5) == expected
+
+  test "sixth day six geese-a-laying":
+    const expected = "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(6) == expected
+
+  test "seventh day seven swans-a-swimming":
+    const expected = "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(7) == expected
+
+  test "eighth day eight maids-a-milking":
+    const expected = "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(8) == expected
+
+  test "ninth day nine ladies dancing":
+    const expected = "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(9) == expected
+
+  test "tenth day ten lords-a-leaping":
+    const expected = "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(10) == expected
+
+  test "eleventh day eleven pipers piping":
+    const expected = "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(11) == expected
+
+  test "twelfth day twelve drummers drumming":
+    const expected = "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(12) == expected
+
+  # Multiple verses: two newline characters between verses, and zero at the end.
+  test "recites first three verses of the song":
+    const expected = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n\n" &
+                     "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(1, 3) == expected
+
+  test "recites three verses from the middle of the song":
+    const expected = "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(4, 6) == expected
+
+  test "recites the whole song":
+    const expected = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n\n" &
+                     "On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n\n" &
+                     "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    check recite(1, 12) == expected
