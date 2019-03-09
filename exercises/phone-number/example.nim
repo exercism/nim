90c066b59f0ce432666e7e34b121a769565f253b
ee7 <45465154+ee7@users.noreply.github.com>
2019-03-09 20:00:05 +0100

    Implement exercise: phone-number (#186)

---
+++ exercises/phone-number/example.nim
@@ -0,0 +1,10 @@
+func clean*(s: string): string =
+  for c in s:
+    if c in {'0'..'9'}:
+      result &= c
+
+  if result.len == 11 and result[0] == '1':
+    result = result[1 .. ^1]
+
+  if result.len != 10 or result[0] in {'0', '1'} or result[3] in {'0', '1'}:
+    raise newException(ValueError, "Invalid phone number: " & s)
---
+++ exercises/phone-number/phone_number_test.nim
@@ -0,0 +1,72 @@
+import unittest
+import phone_number
+
+# version 1.7.0
+
+suite "Phone Number":
+  test "cleans the number":
+    check clean("(223) 456-7890") == "2234567890"
+
+  test "cleans numbers with dots":
+    check clean("223.456.7890") == "2234567890"
+
+  test "cleans numbers with multiple spaces":
+    check clean("223 456   7890   ") == "2234567890"
+
+  test "invalid when 9 digits":
+    expect ValueError:
+      discard clean("123456789")
+
+  test "invalid when 11 digits does not start with a 1":
+    expect ValueError:
+      discard clean("22234567890")
+
+  test "valid when 11 digits and starting with 1":
+    check clean("12234567890") == "2234567890"
+
+  test "valid when 11 digits and starting with 1 even with punctuation":
+    check clean("+1 (223) 456-7890") == "2234567890"
+
+  test "invalid when more than 11 digits":
+    expect ValueError:
+      discard clean("321234567890")
+
+  test "invalid with letters":
+    expect ValueError:
+      discard clean("123-abc-7890")
+
+  test "invalid with punctuations":
+    expect ValueError:
+      discard clean("123-@:!-7890")
+
+  test "invalid if area code starts with 0":
+    expect ValueError:
+      discard clean("(023) 456-7890")
+
+  test "invalid if area code starts with 1":
+    expect ValueError:
+      discard clean("(123) 456-7890")
+
+  test "invalid if exchange code starts with 0":
+    expect ValueError:
+      discard clean("(223) 056-7890")
+
+  test "invalid if exchange code starts with 1":
+    expect ValueError:
+      discard clean("(223) 156-7890")
+
+  test "invalid if area code starts with 0 on valid 11-digit number":
+    expect ValueError:
+      discard clean("1 (023) 456-7890")
+
+  test "invalid if area code starts with 1 on valid 11-digit number":
+    expect ValueError:
+      discard clean("1 (123) 456-7890")
+
+  test "invalid if exchange code starts with 0 on valid 11-digit number":
+    expect ValueError:
+      discard clean("1 (223) 056-7890")
+
+  test "invalid if exchange code starts with 1 on valid 11-digit number":
+    expect ValueError:
+      discard clean("1 (223) 156-7890")
