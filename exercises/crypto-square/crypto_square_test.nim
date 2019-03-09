5db2b1f5817b2911664f6d9ac2b0e943033d3f2b
ee7 <45465154+ee7@users.noreply.github.com>
2019-03-09 20:00:02 +0100

    Implement exercise: crypto-square (#183)

---
+++ exercises/crypto-square/crypto_square_test.nim
@@ -0,0 +1,28 @@
+import unittest
+import crypto_square
+
+# version 3.2.0
+
+suite "Crypto Square":
+  test "empty plaintext results in an empty ciphertext":
+    check encrypt("") == ""
+
+  test "lowercase":
+    check encrypt("A") == "a"
+
+  test "remove spaces":
+    check encrypt("  b ") == "b"
+
+  test "remove punctuation":
+    check encrypt("@1,%!") == "1"
+
+  test "9 character plaintext results in 3 chunks of 3 characters":
+    check encrypt("This is fun!") == "tsf hiu isn"
+
+  test "8 character plaintext results in 3 chunks, the last one with a trailing space":
+    check encrypt("Chill out.") == "clu hlt io "
+
+  test "54 character plaintext results in 7 chunks, the last two with trailing spaces":
+    const pt = "If man was meant to stay on the ground, god would have given us roots."
+    const expected = "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau "
+    check encrypt(pt) == expected
---
+++ exercises/crypto-square/example.nim
@@ -0,0 +1,21 @@
+import math, strutils
+
+func encrypt*(s: string): string =
+  var normalized = newStringOfCap(s.len)
+  for c in s.toLowerAscii():
+    if c in {'a'..'z'} or c in {'0'..'9'}:
+      normalized &= c
+
+  let r = normalized.len.float.sqrt.ceil.int
+  let c = (normalized.len.float / r.float).round.int
+  normalized = normalized.alignLeft(r*c, ' ')
+
+  var ct = newSeq[string]()
+  for s in 0 ..< r:
+    var word = ""
+    for i in countup(s, normalized.high, r):
+      word &= normalized[i]
+
+    ct &= word
+
+  result = ct.join(" ")
