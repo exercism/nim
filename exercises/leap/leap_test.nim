a9ef46b1393716da0455072e8525458cabc613b3
ee7 <45465154+ee7@users.noreply.github.com>
2019-07-16 20:00:03 +0200

    Test(leap): 1.5.1

--- exercises/leap/leap_test.nim
+++ exercises/leap/leap_test.nim
@@ -1,23 +1,23 @@
 import unittest
 import leap
 
-# version 1.5.0
+# version 1.5.1
 
 suite "Leap":
-  test "year not divisible by 4: common year":
+  test "year not divisible by 4 in common year":
     check isLeapYear(2015) == false
 
-  test "year divisible by 2, not divisible by 4: common year":
+  test "year divisible by 2, not divisible by 4 in common year":
     check isLeapYear(1970) == false
 
-  test "year divisible by 4, not divisible by 100: leap year":
+  test "year divisible by 4, not divisible by 100 in leap year":
     check isLeapYear(1996) == true
 
-  test "year divisible by 100, not divisible by 400: common year":
+  test "year divisible by 100, not divisible by 400 in common year":
     check isLeapYear(2100) == false
 
-  test "year divisible by 400: leap year":
+  test "year divisible by 400 in leap year":
     check isLeapYear(2000) == true
 
-  test "year divisible by 200, not divisible by 400: common year":
+  test "year divisible by 200, not divisible by 400 in common year":
     check isLeapYear(1800) == false
