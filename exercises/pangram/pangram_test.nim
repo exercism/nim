427d2a936ce4233611807b90c8a505e891c6d0b0
ee7 <45465154+ee7@users.noreply.github.com>
2019-07-16 20:00:14 +0200

    Test(pangram): 2.0.0

--- exercises/pangram/pangram_test.nim
+++ exercises/pangram/pangram_test.nim
@@ -1,35 +1,35 @@
 import unittest
 import pangram
 
-# version 1.4.1
+# version 2.0.0
 
 suite "Pangram":
-  test "sentence empty":
+  test "empty sentence":
     check isPangram("") == false
 
-  test "recognizes a perfect lower case pangram":
+  test "perfect lower case":
     check isPangram("abcdefghijklmnopqrstuvwxyz") == true
 
-  test "pangram with only lower case":
+  test "only lower case":
     check isPangram("the quick brown fox jumps over the lazy dog") == true
 
-  test "missing character 'x'":
+  test "missing the letter 'x'":
     check isPangram("a quick movement of the enemy will jeopardize five gunboats") == false
 
-  test "another missing character, e.g. 'h'":
+  test "missing the letter 'h'":
     check isPangram("five boxing wizards jump quickly at it") == false
 
-  test "pangram with underscores":
+  test "with underscores":
     check isPangram("the_quick_brown_fox_jumps_over_the_lazy_dog") == true
 
-  test "pangram with numbers":
+  test "with numbers":
     check isPangram("the 1 quick brown fox jumps over the 2 lazy dogs") == true
 
   test "missing letters replaced by numbers":
     check isPangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog") == false
 
-  test "pangram with mixed case and punctuation":
+  test "mixed case and punctuation":
     check isPangram("\"Five quacking Zephyrs jolt my wax bed.\"") == true
 
-  test "upper and lower case versions of the same character should not be counted separately":
+  test "case insensitive":
     check isPangram("the quick brown fox jumps over with lazy FX") == false
