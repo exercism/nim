ce01715db97b6156368cd8384bb72b1e10b43581
ee7 <45465154+ee7@users.noreply.github.com>
2019-07-16 20:00:15 +0200

    Test(darts): 2.2.0

--- exercises/darts/darts_test.nim
+++ exercises/darts/darts_test.nim
@@ -1,26 +1,44 @@
 import unittest
 import darts
 
-# version 1.1.0
+# version 2.2.0
 
 suite "Darts":
-  test "A dart lands outside the target":
+  test "missed target":
     check score((-9.0, 9.0)) == 0
 
-  test "A dart lands just in the border of the target":
+  test "on the outer circle":
     check score((0.0, 10.0)) == 1
 
-  test "A dart lands in the outer circle":
-    check score((4.0, 4.0)) == 1
+  test "on the middle circle":
+    check score((-5.0, 0.0)) == 5
 
-  test "A dart lands right in the border between outer and middle circles":
-    check score((5.0, 0.0)) == 5
-
-  test "A dart lands in the middle circle":
-    check score((0.8, -0.8)) == 5
-
-  test "A dart lands right in the border between middle and inner circles":
+  test "on the inner circle":
     check score((0.0, -1.0)) == 10
 
-  test "A dart lands in the inner circle":
+  test "exactly on centre":
+    check score((0.0, 0.0)) == 10
+
+  test "near the centre":
     check score((-0.1, -0.1)) == 10
+
+  test "just within the inner circle":
+    check score((0.7, 0.7)) == 10
+
+  test "just outside the inner circle":
+    check score((0.8, -0.8)) == 5
+
+  test "just within the middle circle":
+    check score((-3.5, 3.5)) == 5
+
+  test "just outside the middle circle":
+    check score((-3.6, -3.6)) == 1
+
+  test "just within the outer circle":
+    check score((-7.0, 7.0)) == 1
+
+  test "just outside the outer circle":
+    check score((7.1, -7.1)) == 0
+
+  test "asymmetric position between the inner and middle circles":
+    check score((0.5, -4.0)) == 5
