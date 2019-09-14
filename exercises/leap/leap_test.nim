0dc9409bcbe866b8a159d7d9fa441b460cd690b4
ee7 <45465154+ee7@users.noreply.github.com>
2019-09-14 10:00:05 +0200

    Test(leap): 1.6.0
    Upstream:
    - https://github.com/exercism/problem-specifications/commit/f8aaffb

--- exercises/leap/leap_test.nim
+++ exercises/leap/leap_test.nim
@@ -1,7 +1,7 @@
 import unittest
 import leap
 
-# version 1.5.1
+# version 1.6.0
 
 suite "Leap":
   test "year not divisible by 4 in common year":
@@ -13,11 +13,20 @@ suite "Leap":
   test "year divisible by 4, not divisible by 100 in leap year":
     check isLeapYear(1996) == true
 
+  test "year divisible by 4 and 5 is still a leap year":
+    check isLeapYear(1960) == true
+
   test "year divisible by 100, not divisible by 400 in common year":
     check isLeapYear(2100) == false
 
+  test "year divisible by 100 but not by 3 is still not a leap year":
+    check isLeapYear(1900) == false
+
   test "year divisible by 400 in leap year":
     check isLeapYear(2000) == true
+
+  test "year divisible by 400 but not by 125 is still a leap year":
+    check isLeapYear(2400) == true
 
   test "year divisible by 200, not divisible by 400 in common year":
     check isLeapYear(1800) == false
