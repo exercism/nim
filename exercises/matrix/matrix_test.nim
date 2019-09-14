8ad60913946367e469b854d68015158ca04c470b
ee7 <45465154+ee7@users.noreply.github.com>
2019-09-14 10:00:04 +0200

    Test(matrix): 1.3.0
    Upstream:
    - https://github.com/exercism/problem-specifications/commit/f3cb674
    - https://github.com/exercism/problem-specifications/commit/9891a29

--- exercises/matrix/matrix_test.nim
+++ exercises/matrix/matrix_test.nim
@@ -1,7 +1,7 @@
 import unittest
 import matrix
 
-# version 1.1.0
+# version 1.3.0
 
 suite "Matrix":
   test "extract row from one number matrix":
@@ -13,8 +13,8 @@ suite "Matrix":
   test "extract row where numbers have different widths":
     check row("1 2\n10 20", 2) == @[10, 20]
 
-  test "can extract row from non-square matrix":
-    check row("1 2 3\n4 5 6\n7 8 9\n8 7 6", 3) == @[7, 8, 9]
+  test "can extract row from non-square matrix with no corresponding column":
+    check row("1 2 3\n4 5 6\n7 8 9\n8 7 6", 4) == @[8, 7, 6]
 
   test "extract column from one number matrix":
     check column("1", 1) == @[1]
@@ -22,8 +22,8 @@ suite "Matrix":
   test "can extract column":
     check column("1 2 3\n4 5 6\n7 8 9", 3) == @[3, 6, 9]
 
-  test "can extract column from non-square matrix":
-    check column("1 2 3\n4 5 6\n7 8 9\n8 7 6", 3) == @[3, 6, 9, 6]
+  test "can extract column from non-square matrix with no corresponding row":
+    check column("1 2 3 4\n5 6 7 8\n9 8 7 6", 4) == @[4, 8, 6]
 
   test "extract column where numbers have different widths":
     check column("89 1903 3\n18 3 1\n9 4 800", 2) == @[1903, 3, 4]
