dec4b6372e47c38b672c0d8adcdc90981ce15137
ee7 <45465154+ee7@users.noreply.github.com>
2019-10-11 20:00:02 +0200

    CI(check_exercises): Support stubs; Speed up
    This commit rewrites `check_exercises.nim`, which the Nim track uses to
    check that the example solution for every implemented exercise passes
    that exercise's test suite.
    
    Summary:
    - Support stub files. They will be added in a future commit so that
      users will no longer need to create their solution file manually.
    - Speed up testing multiple exercises. Travis CI now compiles all tests
      in 20-30 seconds, not 120-150 seconds. This means that CI builds take
      40-60 seconds, not 150-210 seconds.
    - Improve general usability and convenience.
    
    Implementation:
    - To support stub files: `check_exercises.nim` now copies examples and
      tests to a new directory, instead of copying and renaming
      `example.nim` within each exercise directory.
    - For speed: it now joins all the tests, so that `system.nim` and other
      dependencies only need to be compiled once, rather than per exercise.
      This was the main bottleneck when testing multiple exercises.
    
    Other benefits of this implementation:
    - Improves output readability: all compiler warnings and hints are now
      printed at the top of the log.
    - Doesn't create a binary in every exercise directory.
    - Allows one keyboard interrupt to stop all the tests.
    
    Other changes:
    - Allow running `check_exercises.nim` from any directory.
    - Compile tests with the `--styleCheck:hint` option.
    - No longer compile tests with the `--verbosity=0` option.
    - Use only one `suite` heading per exercise in the output log.
    - Rewrite test suite names from Title Case to exercise-slug format.
    - Avoid using `programResult`, which is deprecated.
    - Improve the error message for unrecognized exercise name arguments.
    
    Closes: #87

--- _test/check_exercises.nim
+++ _test/check_exercises.nim
@@ -1,42 +1,185 @@
-import
-  os, osproc, strutils, sequtils
+import critbits, os, osproc, parseopt, strutils
 
-type TTestResult = tuple[exercise: string, exitCode: int]
+## This file is for testing the Nim track of exercism.io.
+##
+## It checks that the example solution for every implemented exercise passes
+## that exercise's test suite.
+##
+##
+## Usage
+## =====
+## Test all the exercises:
+## - `nim c -r check_exercises.nim`
+##
+## Test a selection of exercises:
+## - `nim c -r check_exercises.nim [exercise-name] [...]`
+##
+##
+## Implementation
+## ==============
+## Running this file will:
+## 1) Copy tests and examples from the standardized Exercism directory structure
+##    into an output directory with a valid Nimble structure.
+##
+## 2) Write the following files:
+##    - `all_tests.nim` (imports every test).
+##    - `config.nims`   (modifies the path for the tests).
+##
+## 3) Run `all_tests.nim`.
+##
+##
+## Output directory structure
+## ==========================
+## ```
+## check_exercises_tmp
+## ├── src
+## │   └── check_exercises
+## │       ├── acronym.nim
+## │       ...
+## │       └── yacht.nim
+## ├── tests
+## │   ├── all_tests.nim
+## │   ├── config.nims
+## │   ├── test_acronym.nim
+## │   ...
+## │   └── test_yacht.nim
+## ```
 
-proc runTest(testFilePath: string): TTestResult =
-  let
-    exerciseDirPath = os.splitPath(testFilePath)[0]
-    exerciseName = os.splitPath(exerciseDirPath)[1]
-    moduleName = os.splitPath(testFilePath).tail.replace("_test", "")
-    examplePath = os.joinPath(exerciseDirPath, "example.nim")
-    modulePath = os.joinPath(exerciseDirPath, moduleName)
+let
+  appDir = getAppDir()
+  exercisesDir = appDir / "../exercises"
+  outDir = appDir / "check_exercises_tmp"
+  testDir = outDir / "tests"
+  srcDir = outDir / "src" / "check_exercises"
+  allTestsPath = testDir / "all_tests.nim"
 
-  os.copyFile(examplePath, modulePath)
-  echo "\n ### " & exerciseName & " ###"
-  let exitCode = osproc.execCmd("nim c -r --verbosity=0 " & testFilePath)
-  os.removeFile(modulePath)
-  (exerciseName, exitCode)
+# Let us define the exercise names as a set of strings. This is simpler than
+# defining an `enum` of all exercises (or all implemented exercises). We can use
+# `CritBitTree[void]` - an efficient container for a sorted set of strings.
+type
+  Slugs = CritBitTree[void]
+
+proc getImplementedSlugs: Slugs =
+  ## Returns the names of the implemented exercises.
+  ##
+  ## Let us consider an "implemented exercise" as one with a correctly named
+  ## test file, rather than one with an entry in `config.json`. This can be more
+  ## convenient when implementing new exercises.
+  for _, dir in walkDir(exercisesDir):
+    for file in walkFiles(dir / "*_test.nim"):
+      result.incl(dir.splitPath().tail) # e.g. "hello-world"
+
+proc prepareDir =
+  ## Creates the new directory structure for the tests.
+  removeDir(outDir)
+  createDir(testDir)
+  createDir(srcDir)
+  const configFileContents = "--path: \"$projectDir/../src/check_exercises\""
+  writeFile(testDir / "config.nims", configFileContents)
+
+proc wrapTest(file: string, slug: string): string =
+  ## Returns the contents of `file`, but with the tests wrapped inside a proc.
+  ##
+  ## This is a workaround for the "too many global variables" error when running
+  ## many top-level tests with `unittest`. It allows us to keep top-level
+  ## `suite` statements in the repository's test files, which keeps them as
+  ## clear as possible for the user.
+  ##
+  ## We need this workaround as the `suite` and `test` templates in `unittest`
+  ## otherwise put every variable in the global scope, and Nim's GC sets a limit
+  ## of 3500 global variables.
+  var inSuite = false
+  let origFile = readFile(file)
+  let numSuites = origFile.count("\nsuite \"")
+  # Allocate a longer string for the wrapped tests.
+  result = newStringOfCap((origFile.len.float * 1.15).int)
+
+  # Add one indentation layer to all lines from "suite" onwards.
+  for line in lines(file):
+    if line.len == 0:
+      result &= "\n"
+    elif line.startsWith("suite \""):
+      # Put all the tests for an exercise into one suite.
+      if not inSuite:
+        inSuite = true
+        result &= "proc main =\n"
+        result &= "  suite \"" & slug & "\":\n"
+      # If there are multiple suites, keep the suite names as comments only.
+      if numSuites > 1:
+        result &= "    # " & line[7 .. ^3] & "\n"
+    elif inSuite:
+      result &= "  " & line & "\n"
+    else:
+      result &= line & "\n"
+  result &= "\nmain()\n"
+  # The below suppresses an "unused import" warning that is otherwise generated
+  # for each exercise. We run each module's `main` proc when importing, but we
+  # don't export any of its symbols.
+  result &= "{.used.}\n"
+
+proc prepareTests(slugs: Slugs) =
+  ## Copies the example solution and a wrapped test file for the exercises in
+  ## `slugs`, and writes a file that joins all the tests for these exercises.
+  ##
+  ## This allows us to compile `system.nim` and other dependencies only once,
+  ## rather than per-exercise, which fixes the main performance bottleneck when
+  ## testing multiple exercises. It also improves convenience by printing all
+  ## compiler warnings and hints at the top of the output.
+  var allTests = "import ../tests/[\n"
+
+  for slug in slugs:
+    let slugUnder = slug.replace("-", "_")
+    let testName = "test_" & slugUnder # e.g. "test_hello_world"
+    allTests &= "  " & testName & ",\n"
+    let dir = exercisesDir / slug
+
+    # Copy and rename the example solution. For example:
+    #   `exercises/bob/example.nim`  ->  `outDir/src/check_exercises/bob.nim`
+    copyFile(dir / "example.nim", srcDir / slugUnder & ".nim")
+
+    # Copy a wrapped version of the test. For example:
+    #   `exercises/bob/bob_test.nim`  ->  `outDir/tests/test_bob.nim`
+    let wrappedTest = wrapTest(dir / slugUnder & "_test.nim", slug)
+    writeFile(testDir / testName & ".nim", wrappedTest)
+
+  allTests &= "]\n"
+  writeFile(allTestsPath, allTests)
+
+proc runTests(slugs: Slugs): int =
+  ## Runs the tests for the exercises in `slugs`.
+  ##
+  ## Returns the exit code, which is `0` if all tests pass and `1` otherwise.
+  prepareDir()
+  prepareTests(slugs)
+
+  result = execCmd("nim c -r --styleCheck:hint " & allTestsPath)
+  if result == 0:
+    let wording = if slugs.len == 1: " exercise." else: " exercises."
+    echo "\nTested ", slugs.len, wording, "\nAll tests passed."
+  else:
+    echo "\nFailure. At least one test failed."
+
+proc parseCmdLine: Slugs =
+  ## Returns the user-specified exercise slugs.
+  let implementedSlugs = getImplementedSlugs()
+
+  for kind, key, val in getopt():
+    case kind
+    of cmdShortOption, cmdLongOption:
+      discard
+    of cmdArgument:
+      if key in implementedSlugs:
+        result.incl(key) # Test specified exercises in the order given.
+      else:
+        echo "Error: unrecognized exercise name: '" & key & "'"
+        quit(0)
+    of cmdEnd: assert(false) # Cannot happen.
+
+  if result.len == 0:
+    result = implementedSlugs # Test all exercises (in alphabetical order).
 
 when isMainModule:
-  var
-    exercises: seq[string] = @[]
-    failures: seq[string] = @[]
-
-  let arguments = commandLineParams()
-
-  if arguments.len == 0:
-    exercises.add("*")
-  else:
-    exercises = arguments
-
-  for exercise in exercises:
-    for file in os.walkFiles(os.joinPath("./exercises", exercise, "*_test.nim")):
-      let (exercise, exitCode) = runTest(file)
-      if exitCode != 0:
-        failures.add(exercise)
-
-  if failures.len == 0:
-    echo "SUCCESS"
-  else:
-    echo "FAILURES: " & failures.join(", ")
-    programResult = 1
+  let slugs = parseCmdLine()
+  let exitCode = runTests(slugs)
+  if exitCode != 0:
+    quit(exitCode)
