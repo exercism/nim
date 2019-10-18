265398735aca7f55e9628a179f83f11e19703d7a
ee7 <45465154+ee7@users.noreply.github.com>
2019-10-18 20:00:01 +0200

    Feature(resistor-color-trio): Implement exercise (#216)
    Decisions:
    - Implement the input type as an `array`, not a `seq`.
    - Support only `tuple` (and not `object`) as the return type.
    - `example.nim`: Support megaohms and gigaohms.
    - `example.nim`: Omit creating a compile-time lookup.
    
    Upstream:
    - https://github.com/exercism/problem-specifications/commit/c0ecfc2

---
+++ exercises/resistor-color-trio/example.nim
@@ -0,0 +1,20 @@
+import math
+
+type
+  ResistorColor* = enum
+    Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White
+  Resistor = array[3, ResistorColor]
+
+func ohms(a: Resistor): int =
+  (10 * a[0].ord + a[1].ord) * 10^(a[2].ord)
+
+func label*(a: Resistor): tuple[value: int, unit: string] =
+  result.value = ohms(a)
+  const units = ["ohms", "kiloohms", "megaohms", "gigaohms"]
+
+  for i in 0 .. units.high:
+    if result.value >= 1000:
+      result.value = result.value div 1000
+    else:
+      result.unit = units[i]
+      return
---
+++ exercises/resistor-color-trio/resistor_color_trio_test.nim
@@ -0,0 +1,20 @@
+import unittest
+import resistor_color_trio
+
+# version 1.0.0
+
+suite "Resistor Color Trio":
+  test "orange and orange and black":
+    check label([Orange, Orange, Black]) == (33, "ohms")
+
+  test "blue and grey and brown":
+    check label([Blue, Grey, Brown]) == (680, "ohms")
+
+  test "red and black and red":
+    check label([Red, Black, Red]) == (2, "kiloohms")
+
+  test "green and brown and orange":
+    check label([Green, Brown, Orange]) == (51, "kiloohms")
+
+  test "yellow and violet and yellow":
+    check label([Yellow, Violet, Yellow]) == (470, "kiloohms")
