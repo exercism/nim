45d072a78c3f1a10eb71c635d8be154bebf48585
ee7 <45465154+ee7@users.noreply.github.com>
2019-11-28 18:00:03 +0100

    CI(Travis): Tweak paths and `curl` options
    This commit tweaks the commands that download and extract the Nim
    release archive.
    
    Changes:
    - Use absolute paths and remove `cd`.
    - curl: Retry download in case of failure, and follow redirects.

--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -4,9 +4,13 @@
 
 set -eux
 
-NIM_URL="https://nim-lang.org/download/nim-${NIM_VERSION}-linux_x64.tar.xz"
-NIM_TAR="nim-${NIM_VERSION}.tar.xz"
+BIN_DIR="${TRAVIS_BUILD_DIR}/bin"
 
-cd "${TRAVIS_BUILD_DIR}/bin"
-curl -sSf "${NIM_URL}" > "${NIM_TAR}"
-tar xf "${NIM_TAR}"
+# Download the stable release archive that contains pre-built binaries.
+NIM_URL="https://nim-lang.org/download/nim-${NIM_VERSION}-linux_x64.tar.xz"
+NIM_EXTRACT_DIR="${BIN_DIR}/nim-${NIM_VERSION}"
+NIM_TAR="${NIM_EXTRACT_DIR}.tar.xz"
+curl -sSfL --retry 3 --retry-connrefused "${NIM_URL}" -o "${NIM_TAR}"
+
+# Extract the archive.
+tar -C "${BIN_DIR}" -xf "${NIM_TAR}"
