3863f1e9527606fcc22c9758a899381c74e83c35
ee7 <45465154+ee7@users.noreply.github.com>
2020-01-15 20:00:13 +0100

    CI: Cache the Nim installation
    The runtime of `bin/install-nim.sh` on Travis CI was typically about:
    - 5 minutes for amd64 + Linux
    - 4 minutes for amd64 + macOS
    - 7 minutes for arm64 + Linux
    - 4 minutes for ppc64le + Linux
    
    Therefore caching the Nim installation reduces the build durations by
    about the above amounts when there is no update for the given Nim
    `CHANNEL`.
    
    Example cache contents (for Linux on amd64 with Nim 1.0.4):
    4.7M  ./bin/nim-stable/.git
    5.0M  ./bin/nim-stable/bin/nim
    1.1M  ./bin/nim-stable/bin/nimble
    39K   ./bin/nim-stable/config
    4.3M  ./bin/nim-stable/lib
    ----
    15M   ./bin/nim-stable
    
    This produces a Travis-reported compressed cache size of about 8 MB per
    job in the build matrix.
    
    Decisions:
    - Show the cache contents, with apparent file sizes.
    - Reduce the disk usage of the Nim installation and the `.git` folder to
      reduce the cache size, and do that within the install script, not
      in `before_cache`.
    - Don't bother to reduce the cache size further. It would be easy to
      cache only the `.git` directory and the Nim/Nimble binaries (by
      removing the `lib` directory before caching and restoring it each run
      via `git restore` or similar).

--- bin/install-nim.sh
+++ bin/install-nim.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 #
-# This script installs Nim to the directory named `nim-${CHANNEL}` in the same
-# directory as this script.
+# This script ensures that there is an up-to-date Nim installation in the
+# directory named `nim-${CHANNEL}` in the same directory as this script.
+# The script updates an existing installation or performs a clean install as
+# needed.
 #
 # The environment variable `CHANNEL` determines the Nim version as follows:
 # - If `CHANNEL` is `stable`: install from the latest Nim tag.
@@ -13,6 +15,10 @@ rebuild() {
   bin/nim c --skipUserCfg --skipParentCfg koch.nim
   ./koch boot -d:release
   ./koch nimble
+  # Reduce the cache size by removing unnecessary files.
+  find . ! -name . -prune \
+    ! -name '.git' ! -name 'bin' ! -name 'config' ! -name 'lib' \
+    -exec rm -rf {} +
 }
 
 if [ "${CHANNEL}" = 'stable' ]; then
@@ -25,14 +31,40 @@ readonly NIM_DIR="nim-${CHANNEL}"
 
 cd -- "$(dirname -- "$0")" # Change directory to that of this script.
 
-git clone -b "${BRANCH_OR_TAG}" --depth 1 https://github.com/nim-lang/Nim.git "${NIM_DIR}"
-(
-  cd "${NIM_DIR}"
-  git clone --depth 1 https://github.com/nim-lang/csources.git
+if [ ! -x "${NIM_DIR}/bin/nim" ] || [ ! -x "${NIM_DIR}/bin/nimble" ]; then
+  # At least one of the binaries was not restored from the cache. Build them.
+  rm -rf "${NIM_DIR}"
+  git clone -b "${BRANCH_OR_TAG}" --depth 1 https://github.com/nim-lang/Nim.git "${NIM_DIR}"
   (
-    cd csources
-    sh build.sh
+    cd "${NIM_DIR}"
+    git clone --depth 1 https://github.com/nim-lang/csources.git
+    (
+      cd csources
+      sh build.sh
+    )
+    rm -rf csources
+    rebuild
   )
-  rm -rf csources
-  rebuild
-)
+else
+  # Nim and Nimble were in the cache. Rebuild them if there is a newer tag/commit.
+  (
+    cd "${NIM_DIR}"
+
+    if [ "${CHANNEL}" = 'stable' ]; then
+      git fetch --depth 1 origin tag "${BRANCH_OR_TAG}"
+    else
+      git fetch --depth 1 origin "${BRANCH_OR_TAG}"
+    fi
+
+    if ! git diff --quiet HEAD FETCH_HEAD; then
+      git reset --hard FETCH_HEAD
+      rebuild
+      # Pretend that we did a fresh shallow clone of just the latest tag/commit.
+      if [ "${CHANNEL}" = 'stable' ]; then
+        git tag -d "$(git tag --list --sort=-creatordate | tail -n 1)" # Delete the older tag.
+      fi
+      git reflog expire --expire=all --all # Clear the reflog.
+      git gc --prune=now
+    fi
+  )
+fi
