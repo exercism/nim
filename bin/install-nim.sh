9ea1990342a28a39194799e9b39018563e3bbe52
ee7 <45465154+ee7@users.noreply.github.com>
2020-01-15 20:00:09 +0100

    CI: Install Nim from source, not pre-built binaries
    This means the install script is no longer restricted to Linux and
    amd64.
    
    Decisions:
    - Build Nim via `git clone`, rather than downloading a source tarball.
      Then we can use the same approach to also build from the `devel`
      branch.
    - Don't use `choosenim`. One reason is that it doesn't currently support
      arm64, which is now available with Travis CI.
    - Don't use Docker.
    - Clone the tag directly, rather cloning the `version-1-0` branch and
      checking out the tag.
    - Perform a shallow clone. This finishes faster and allows the `.git`
      directory to be 5 MB, not 80 MB, reducing the future cache size.
    - Use `build.sh`, not `build_all.sh`. The latter is slower because it
      uses `./koch tools` when we only currently need `./koch nimble`.

--- bin/install-nim.sh
+++ bin/install-nim.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
-
-# Travis CI uses this script to install Nim from the pre-built binaries.
+#
+# This script installs the Nim version given by the `NIM_VERSION` environment variable
+# into a directory named `nim-${NIM_VERSION}` in the same directory as this script.
 
 set -eux
 
+rebuild() {
+  bin/nim c --skipUserCfg --skipParentCfg koch.nim
+  ./koch boot -d:release
+  ./koch nimble
+}
+
+readonly NIM_DIR="nim-${NIM_VERSION}"
+
 cd -- "$(dirname -- "$0")" # Change directory to that of this script.
 
-# Download the stable release archive that contains pre-built binaries.
-NIM_URL="https://nim-lang.org/download/nim-${NIM_VERSION}-linux_x64.tar.xz"
-NIM_DIR="nim-${NIM_VERSION}"
-NIM_TAR="${NIM_DIR}.tar.xz"
-curl -sSfL --retry 3 --retry-connrefused "${NIM_URL}" -o "${NIM_TAR}"
-
-# Extract the archive.
-tar -xf "${NIM_TAR}"
+git clone -b "v${NIM_VERSION}" --depth 1 https://github.com/nim-lang/Nim.git "${NIM_DIR}"
+(
+  cd "${NIM_DIR}"
+  git clone --depth 1 https://github.com/nim-lang/csources.git
+  (
+    cd csources
+    sh build.sh
+  )
+  rm -rf csources
+  rebuild
+)
