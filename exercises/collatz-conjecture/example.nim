6fe707c19f910e5eaf5a63a9ea49632eaa692868
ee7 <45465154+ee7@users.noreply.github.com>
2020-05-05 20:00:08 +0200

    Style(collatz-conjecture): Remove semicolon

--- exercises/collatz-conjecture/example.nim
+++ exercises/collatz-conjecture/example.nim
@@ -2,7 +2,7 @@ proc steps*(number: int): int =
   if (number < 1):
     raise newException(ValueError, "Only positive numbers are allowed")
 
-  var n = number;
+  var n = number
   while (n != 1):
     if (n mod 2 == 0):
       n = (n / 2).toInt
