89a1a99f5e8427ef10a623270fc1022a33c3610a
ee7 <45465154+ee7@users.noreply.github.com>
2022-08-12 10:00:18 +0200

    exercises(roman-numerals): sync tests

--- exercises/practice/roman-numerals/test_roman_numerals.nim
+++ exercises/practice/roman-numerals/test_roman_numerals.nim
@@ -2,59 +2,74 @@ import unittest
 import roman_numerals
 
 suite "Roman Numerals":
-  test "1 is a single I":
+  test "1 is I":
     check roman(1) == "I"
 
-  test "2 is two I's":
+  test "2 is II":
     check roman(2) == "II"
 
-  test "3 is three I's":
+  test "3 is III":
     check roman(3) == "III"
 
-  test "4, being 5 - 1, is IV":
+  test "4 is IV":
     check roman(4) == "IV"
 
-  test "5 is a single V":
+  test "5 is V":
     check roman(5) == "V"
 
-  test "6, being 5 + 1, is VI":
+  test "6 is VI":
     check roman(6) == "VI"
 
-  test "9, being 10 - 1, is IX":
+  test "9 is IX":
     check roman(9) == "IX"
 
-  test "20 is two X's":
+  test "27 is XXVII":
     check roman(27) == "XXVII"
 
-  test "48 is not 50 - 2 but rather 40 + 8":
+  test "48 is XLVIII":
     check roman(48) == "XLVIII"
 
-  test "49 is not 40 + 5 + 4 but rather 50 - 10 + 10 - 1":
+  test "49 is XLIX":
     check roman(49) == "XLIX"
 
-  test "50 is a single L":
+  test "59 is LIX":
     check roman(59) == "LIX"
 
-  test "90, being 100 - 10, is XC":
+  test "93 is XCIII":
     check roman(93) == "XCIII"
 
-  test "100 is a single C":
+  test "141 is CXLI":
     check roman(141) == "CXLI"
 
-  test "60, being 50 + 10, is LX":
+  test "163 is CLXIII":
     check roman(163) == "CLXIII"
 
-  test "400, being 500 - 100, is CD":
+  test "402 is CDII":
     check roman(402) == "CDII"
 
-  test "500 is a single D":
+  test "575 is DLXXV":
     check roman(575) == "DLXXV"
 
-  test "900, being 1000 - 100, is CM":
+  test "911 is CMXI":
     check roman(911) == "CMXI"
 
-  test "1000 is a single M":
+  test "1024 is MXXIV":
     check roman(1024) == "MXXIV"
 
-  test "3000 is three M's":
+  test "3000 is MMM":
     check roman(3000) == "MMM"
+
+  test "16 is XVI":
+    check roman(16) == "XVI"
+
+  test "66 is LXVI":
+    check roman(66) == "LXVI"
+
+  test "166 is CLXVI":
+    check roman(166) == "CLXVI"
+
+  test "666 is DCLXVI":
+    check roman(666) == "DCLXVI"
+
+  test "1666 is MDCLXVI":
+    check roman(1666) == "MDCLXVI"
