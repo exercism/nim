297e3f65094831f0b600b2e17d224657e5adad4d
ee7 <45465154+ee7@users.noreply.github.com>
2022-08-12 10:00:15 +0200

    exercises(prime-factors): sync tests

--- exercises/practice/prime-factors/test_prime_factors.nim
+++ exercises/practice/prime-factors/test_prime_factors.nim
@@ -8,11 +8,26 @@ suite "Prime Factors":
   test "prime number":
     check primeFactors(2) == @[2]
 
+  test "another prime number":
+    check primeFactors(3) == @[3]
+
   test "square of a prime":
     check primeFactors(9) == @[3, 3]
 
+  test "product of first prime":
+    check primeFactors(4) == @[2, 2]
+
   test "cube of a prime":
     check primeFactors(8) == @[2, 2, 2]
+
+  test "product of second prime":
+    check primeFactors(27) == @[3, 3, 3]
+
+  test "product of third prime":
+    check primeFactors(625) == @[5, 5, 5, 5]
+
+  test "product of first and second prime":
+    check primeFactors(6) == @[2, 3]
 
   test "product of primes and non-primes":
     check primeFactors(12) == @[2, 2, 3]
