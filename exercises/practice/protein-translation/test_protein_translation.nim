31e7ebcc6fb91cfeeebbd0b3c424cd5b65c6182a
ee7 <45465154+ee7@users.noreply.github.com>
2022-08-12 09:23:46 +0200

    exercises(protein-translation): restructure tests (#363)
    Changes:
    - Don't use loops in the earlier tests.
    - Don't declare variables in the later tests.
    - Use upstream test descriptions.

--- exercises/practice/protein-translation/test_protein_translation.nim
+++ exercises/practice/protein-translation/test_protein_translation.nim
@@ -2,61 +2,71 @@ import unittest
 import protein_translation
 
 suite "Protein Translation":
-  test "identifies Methionine codon":
+  test "Methionine RNA sequence":
     check translate("AUG") == @["Methionine"]
 
-  test "identifies Phenylalanine codons":
-    for codon in ["UUU", "UUC"]:
-      check translate(codon) == @["Phenylalanine"]
+  test "Phenylalanine RNA sequence 1":
+    check translate("UUU") == @["Phenylalanine"]
 
-  test "identifies Leucine codons":
-    for codon in ["UUA", "UUG"]:
-      check translate(codon) == @["Leucine"]
+  test "Phenylalanine RNA sequence 2":
+    check translate("UUC") == @["Phenylalanine"]
 
-  test "identifies Serine codons":
-    for codon in ["UCU", "UCC", "UCA", "UCG"]:
-      check translate(codon) == @["Serine"]
+  test "Leucine RNA sequence 1":
+    check translate("UUA") == @["Leucine"]
 
-  test "identifies Tyrosine codons":
-    for codon in ["UAU", "UAC"]:
-      check translate(codon) == @["Tyrosine"]
+  test "Leucine RNA sequence 2":
+    check translate("UUG") == @["Leucine"]
 
-  test "identifies Cysteine codons":
-    for codon in ["UGU", "UGC"]:
-      check translate(codon) == @["Cysteine"]
+  test "Serine RNA sequence 1":
+    check translate("UCU") == @["Serine"]
 
-  test "identifies Tryptophan codon":
+  test "Serine RNA sequence 2":
+    check translate("UCC") == @["Serine"]
+
+  test "Serine RNA sequence 3":
+    check translate("UCA") == @["Serine"]
+
+  test "Serine RNA sequence 4":
+    check translate("UCG") == @["Serine"]
+
+  test "Tyrosine RNA sequence 1":
+    check translate("UAU") == @["Tyrosine"]
+
+  test "Tyrosine RNA sequence 2":
+    check translate("UAC") == @["Tyrosine"]
+
+  test "Cysteine RNA sequence 1":
+    check translate("UGU") == @["Cysteine"]
+
+  test "Cysteine RNA sequence 2":
+    check translate("UGC") == @["Cysteine"]
+
+  test "Tryptophan RNA sequence":
     check translate("UGG") == @["Tryptophan"]
 
-  test "identifies STOP codons":
-    for codon in ["UAA", "UAG", "UGA"]:
-      check translate(codon).len == 0
+  test "STOP codon RNA sequence 1":
+    check translate("UAA").len == 0
 
-  test "translates RNA strand into correct protein":
-    const strand = "AUGUUUUGG"
-    const expected = @["Methionine", "Phenylalanine", "Tryptophan"]
-    check translate(strand) == expected
+  test "STOP codon RNA sequence 2":
+    check translate("UAG").len == 0
 
-  test "stops translation if STOP codon at beginning of sequence":
-    const strand = "UAGUGG"
-    check translate(strand).len == 0
+  test "STOP codon RNA sequence 3":
+    check translate("UGA").len == 0
 
-  test "stops translation if STOP codon at end of two-codon sequence":
-    const strand = "UGGUAG"
-    const expected = @["Tryptophan"]
-    check translate(strand) == expected
+  test "translate RNA strand into correct protein list":
+    check translate("AUGUUUUGG") == @["Methionine", "Phenylalanine", "Tryptophan"]
 
-  test "stops translation if STOP codon at end of three-codon sequence":
-    const strand = "AUGUUUUAA"
-    const expected = @["Methionine", "Phenylalanine"]
-    check translate(strand) == expected
+  test "translation stops if STOP codon at beginning of sequence":
+    check translate("UAGUGG").len == 0
 
-  test "stops translation if STOP codon in middle of three-codon sequence":
-    const strand = "UGGUAGUGG"
-    const expected = @["Tryptophan"]
-    check translate(strand) == expected
+  test "translation stops if STOP codon at end of two-codon sequence":
+    check translate("UGGUAG") == @["Tryptophan"]
 
-  test "stops translation if STOP codon in middle of six-codon sequence":
-    const strand = "UGGUGUUAUUAAUGGUUU"
-    const expected = @["Tryptophan", "Cysteine", "Tyrosine"]
-    check translate(strand) == expected
+  test "translation stops if STOP codon at end of three-codon sequence":
+    check translate("AUGUUUUAA") == @["Methionine", "Phenylalanine"]
+
+  test "translation stops if STOP codon in middle of three-codon sequence":
+    check translate("UGGUAGUGG") == @["Tryptophan"]
+
+  test "translation stops if STOP codon in middle of six-codon sequence":
+    check translate("UGGUGUUAUUAAUGGUUU") == @["Tryptophan", "Cysteine", "Tyrosine"]
