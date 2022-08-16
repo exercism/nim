21e3e9050d6badd792fd66afbea7ed489f7e7ab5
ee7 <45465154+ee7@users.noreply.github.com>
2022-08-16 13:04:28 +0200

    exercises(resistor-color-duo): sync tests (#381)

--- exercises/practice/resistor-color-duo/test_resistor_color_duo.nim
+++ exercises/practice/resistor-color-duo/test_resistor_color_duo.nim
@@ -11,8 +11,14 @@ suite "Resistor Color Duo":
   test "yellow and violet":
     check value([Yellow, Violet]) == 47
 
+  test "white and red":
+    check value([White, Red]) == 92
+
   test "orange and orange":
     check value([Orange, Orange]) == 33
 
   test "ignore additional colors":
     check value([Green, Brown, Orange]) == 51
+
+  test "black and brown, one-digit":
+    check value([Black, Brown]) == 1
