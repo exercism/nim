b770d96b4d1e45f1cba58fdb24ccb00794f6bf53
ee7 <45465154+ee7@users.noreply.github.com>
2023-01-11 12:21:22 +0100

    exercises(space-age): overhaul to use enum (#435)
    Closes: #405

--- exercises/practice/space-age/test_space_age.nim
+++ exercises/practice/space-age/test_space_age.nim
@@ -7,33 +7,29 @@ suite "Space Age":
     abs(x - y) < 0.01
 
   test "age on Earth":
-    check age("Earth", 1_000_000_000) ~= 31.69
+    check age(Earth, 1_000_000_000) ~= 31.69
 
   test "age on Mercury":
-    check age("Mercury", 2_134_835_688) ~= 280.88
+    check age(Mercury, 2_134_835_688) ~= 280.88
 
   test "age on Venus":
-    check age("Venus", 189_839_836) ~= 9.78
+    check age(Venus, 189_839_836) ~= 9.78
 
   test "age on Mars":
-    check age("Mars", 2_129_871_239) ~= 35.88
+    check age(Mars, 2_129_871_239) ~= 35.88
 
   test "age on Jupiter":
-    check age("Jupiter", 901_876_382) ~= 2.41
+    check age(Jupiter, 901_876_382) ~= 2.41
 
   test "age on Saturn":
-    check age("Saturn", 2_000_000_000) ~= 2.15
+    check age(Saturn, 2_000_000_000) ~= 2.15
 
   test "age on Uranus":
-    check age("Uranus", 1_210_123_456) ~= 0.46
+    check age(Uranus, 1_210_123_456) ~= 0.46
 
   test "age on Neptune":
-    check age("Neptune", 1_821_023_456) ~= 0.35
+    check age(Neptune, 1_821_023_456) ~= 0.35
 
   # Bonus
   test "age that requires int64":
-    check age("Saturn", 3_000_000_000) ~= 3.23
-
-  test "invalid planet causes error":
-    expect ValueError:
-      discard age("Sun", 680_804_807)
+    check age(Saturn, 3_000_000_000) ~= 3.23
