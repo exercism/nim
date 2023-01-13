108a04a270123a2010e57c39fb0c833f5851b442
ee7 <45465154+ee7@users.noreply.github.com>
2023-01-13 17:03:35 +0100

    exercises(clock): remove support for older Nim (#469)
    With commit 1ac18dfba497 (".github, docs: bump  minimum Nim version from
    1.0.0 to 1.6.0", 2023-01-12) the Nim track now officially only supports
    Nim 1.6.0 or newer.
    
    CI no longer tests against any older Nim version, so we can remove
    support for such versions from the example solution. This commit isn't
    user-facing.

--- exercises/practice/clock/.meta/example.nim
+++ exercises/practice/clock/.meta/example.nim
@@ -1,14 +1,5 @@
 import std/[math, strformat]
 
-# `euclMod` was added to `std/math` in Nim 1.6.0.
-# Define `euclMod` ourselves when we are compiling with an earlier Nim version.
-when not compiles(euclMod(3, 2)):
-  func euclMod*[T: SomeNumber](x, y: T): T =
-    ## Returns euclidean modulo of `x` by `y` (returning a non-negative number).
-    result = x mod y
-    if result < 0:
-      result += abs(y)
-
 type
   Clock* = object
     hour*: range[0..23]
