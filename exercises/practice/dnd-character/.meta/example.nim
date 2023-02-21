fd7e3ac09deeb4c13e67fe8d257bce36a3693937
ee7 <45465154+ee7@users.noreply.github.com>
2023-02-21 20:00:01 +0100

    exercises: implement dnd-character

---
+++ exercises/practice/dnd-character/.meta/example.nim
@@ -0,0 +1,37 @@
+import std/[math, random]
+
+type
+  Character = object
+    strength*: int
+    dexterity*: int
+    constitution*: int
+    intelligence*: int
+    wisdom*: int
+    charisma*: int
+    hitpoints*: int
+  DiceRange = 1..6
+
+randomize()
+
+proc ability*: int =
+  var lowest = int.high
+  for i in 1..4:
+    let roll = rand(DiceRange)
+    result += roll
+    lowest = min(lowest, roll)
+  result -= lowest
+
+func modifier*(n: int): int =
+  (n - 10).floorDiv(2)
+
+proc initCharacter*: Character =
+  let constitution = ability()
+  Character(
+    strength: ability(),
+    dexterity: ability(),
+    constitution: constitution,
+    intelligence: ability(),
+    wisdom: ability(),
+    charisma: ability(),
+    hitpoints: 10 + modifier(constitution),
+  )
---
+++ exercises/practice/dnd-character/dnd_character.nim
@@ -0,0 +1,11 @@
+type
+  Character = object
+
+proc ability*: int =
+  discard
+
+proc modifier*(n: int): int =
+  discard
+
+proc initCharacter*: Character =
+  discard
