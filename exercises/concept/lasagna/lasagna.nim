957596c010fd513d7a749c1e2c759bc0a783de06
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-05 12:30:01 +0100

    exercises(lasagna): stub: make parameter names match instructions

--- exercises/concept/lasagna/lasagna.nim
+++ exercises/concept/lasagna/lasagna.nim
@@ -1,14 +1,14 @@
 let expectedMinutesInOven* = 5
 
-proc remainingMinutesInOven*(elapsedMinutes: int): int =
+proc remainingMinutesInOven*(actualMinutesInOven: int): int =
   ## Returns the number of minutes that the lasagna still needs to remain in the
   ## oven to be properly prepared.
   discard
 
-proc preparationTimeInMinutes*(layers: int): int =
+proc preparationTimeInMinutes*(numberOfLayers: int): int =
   ## Returns the total preparation time for a given number of layers.
   discard
 
-proc totalTimeInMinutes*(layers, minutesInOven: int): int =
+proc totalTimeInMinutes*(numberOfLayers, actualMinutesInOven: int): int =
   ## Returns the total time required to prepare the lasagna.
   discard
