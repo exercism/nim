351c222f00d01ddc605e5e73fa7b79569e517212
ee7 <45465154+ee7@users.noreply.github.com>
2023-03-05 12:30:05 +0100

    exercises(dnd-character): add Character fields
    I'm not convinced that it's worth requiring the user to add this
    themselves.

--- exercises/practice/dnd-character/dnd_character.nim
+++ exercises/practice/dnd-character/dnd_character.nim
@@ -1,6 +1,12 @@
 type
   Character* = object
     strength*: int
+    dexterity*: int
+    constitution*: int
+    intelligence*: int
+    wisdom*: int
+    charisma*: int
+    hitpoints*: int
 
 proc ability*: int =
   discard
