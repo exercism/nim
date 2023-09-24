3383b8febf7e12193cd2537b83f0f2e2977b660f
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-24 10:00:27 +0200

    refactor example: proverb

--- exercises/practice/proverb/.meta/example.nim
+++ exercises/practice/proverb/.meta/example.nim
@@ -1,10 +1,8 @@
 import std/strformat
 
-func recite*(s: openArray[string]): string =
-  if s.len == 0:
-    return
-
-  for i in 0 .. s.high - 1:
-    result.add &"For want of a {s[i]} the {s[i+1]} was lost.\n"
-
-  result.add &"And all for the want of a {s[0]}."
+func recite*(a: openArray[string]): string =
+  result = ""
+  if a.len > 0:
+    for i in 0 ..< a.high:
+      result.add &"For want of a {a[i]} the {a[i+1]} was lost.\n"
+    result.add &"And all for the want of a {a[0]}."
