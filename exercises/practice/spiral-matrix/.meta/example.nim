7435b302310a1a0a61d436c704c462db7ec30797
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-24 10:00:37 +0200

    refactor example: spiral-matrix

--- exercises/practice/spiral-matrix/.meta/example.nim
+++ exercises/practice/spiral-matrix/.meta/example.nim
@@ -1,23 +1,16 @@
-type Point = tuple[x, y: int]
-
-func `+=`(a: var Point, b: Point) =
-  a = (a.x + b.x, a.y + b.y)
-
-# Define x to increase rightwards, and y to increase downwards.
-const directions: array[4, Point] = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
 func spiral*(n: static int): array[n, array[n, int]] =
   var walkLen = n
   var count = 1
-  var p: Point = (-1, 0)  # Start outside the top left corner.
-
+  var p = (x: -1, y: 0) # Start outside the top left corner.
   while walkLen > 0:
-    for d in directions:  # Walk in the order: right, down, left, up.
+    # Define x to increase rightwards, and y to increase downwards.
+    # Walk in the order: right, down, left, up.
+    for (x, y) in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
       for _ in 1..walkLen:
-        p += d
+        p.x += x
+        p.y += y
         result[p.y][p.x] = count
         inc count
-
       # Subtract 1 from the walk length after finishing a horizontal walk.
-      if d.y == 0:
+      if y == 0:
         dec walkLen
