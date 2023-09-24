359ba556fe2bf541aeb957a30778cc54767bd44c
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-24 10:00:04 +0200

    refactor example: armstrong-numbers

--- exercises/practice/armstrong-numbers/.meta/example.nim
+++ exercises/practice/armstrong-numbers/.meta/example.nim
@@ -1,10 +1,26 @@
-import std/[math, strutils]
+import std/math
 
-func isArmstrongNumber*(input: int): bool =
-  let stringNumber = $input
-  let numberLength = len(stringNumber).toFloat
+func countDigits(n: Natural): int =
+  ## Returns the number of digits in `n`.
+  ## Avoids expensive operations like `div`, `log10`, and string conversion.
+  ## Optimizes for smallest numbers, without binary search.
+  result = 19 # int64.high is approximately 9.2 x 10^18, which has 19 digits.
+  var t = 1
+  for i in 1 ..< result:
+    t *= 10
+    if n < t:
+      return i
 
+iterator digits(n: Natural): int =
+  ## Yields the digits of `n`.
+  var n = n
+  while n > 0:
+    yield n mod 10
+    n = n div 10
+
+func isArmstrongNumber*(n: Natural): bool =
+  let numDigits = countDigits(n)
   var sum = 0
-  for i in stringNumber:
-    sum = sum + pow(parseFloat($i), numberLength).toInt
-  sum == input
+  for d in digits(n):
+    sum += d^numDigits
+  sum == n
