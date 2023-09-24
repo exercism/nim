ed3f53445fa8f775777f52204bd78eb2fa3ccc2d
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-24 10:00:02 +0200

    refactor example: all-your-base

--- exercises/practice/all-your-base/.meta/example.nim
+++ exercises/practice/all-your-base/.meta/example.nim
@@ -1,23 +1,30 @@
-import std/[algorithm, math]
+import std/algorithm
 
-func convert*(digits: openArray[int], fromBase: int, toBase: int): seq[int] =
-  if fromBase < 2:
-    raise newException(ValueError, "Invalid input base: " & $fromBase)
-  if toBase < 2:
-    raise newException(ValueError, "Invalid output base: " & $toBase)
-  for d in digits:
-    if d >= fromBase or d < 0:
-      raise newException(ValueError, "Invalid input digit: " & $d)
+func toBase10(digits: openArray[int], fromBase: int): int =
+  result = 0
+  var power = 1
+  for i in countdown(digits.high, 0):
+    let d = digits[i]
+    result += d * power
+    power *= fromBase
 
-  # Convert to base 10
-  var n: int
-  for i, d in digits:
-    n += d * fromBase^(digits.high - i)
-
-  # Convert base 10 to digits in the output base
+func fromBase10(n: int, toBase: int): seq[int] =
   if n == 0:
     return @[0]
+  result = newSeq[int]()
+  var n = n
   while n > 0:
     result.add n mod toBase
     n = n div toBase
   result.reverse
+
+func convert*(digits: openArray[int], fromBase: int, toBase: int): seq[int] =
+  if fromBase < 2:
+    raise newException(ValueError, "Invalid input base: " & $fromBase)
+  elif toBase < 2:
+    raise newException(ValueError, "Invalid output base: " & $toBase)
+  else:
+    for d in digits:
+      if d >= fromBase or d < 0:
+        raise newException(ValueError, "Invalid input digit: " & $d)
+  digits.toBase10(fromBase).fromBase10(toBase)
