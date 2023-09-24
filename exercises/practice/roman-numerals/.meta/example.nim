eff4e88db9ce0566f66c73cd4e2d2d85eeab5f13
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-24 10:00:31 +0200

    refactor example: roman-numerals

--- exercises/practice/roman-numerals/.meta/example.nim
+++ exercises/practice/roman-numerals/.meta/example.nim
@@ -1,29 +1,14 @@
-type
-  Numeral = tuple[number: int, roman: string]
-  NumeralArray = array[13, Numeral]
+const arabicToRoman = {
+  1000: "M", 900: "CM", 500: "D", 400: "CD",
+   100: "C",  90: "XC",  50: "L",  40: "XL",
+    10: "X",   9: "IX",   5: "V",   4: "IV",
+     1: "I",
+}
 
-let
-  numeralMapping: NumeralArray = [
-    (1000, "M"),
-    (900, "CM"),
-    (500, "D"),
-    (400, "CD"),
-    (100, "C"),
-    (90, "XC"),
-    (50, "L"),
-    (40, "XL"),
-    (10, "X"),
-    (9, "IX"),
-    (5, "V"),
-    (4, "IV"),
-    (1, "I")
-  ]
-
-
-proc roman*(number: int): string =
+func roman*(n: int): string =
   result = ""
-  var n = number
-  for numeral in numeralMapping:
-    while n >= numeral.number:
-      result = result & numeral.roman
-      n = n - numeral.number
+  var n = n
+  for (a, r) in arabicToRoman:
+    while n >= a:
+      result.add r
+      n -= a
