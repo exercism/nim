dac8977e207294273775abb402915119111b372d
ee7 <45465154+ee7@users.noreply.github.com>
2023-09-24 10:00:17 +0200

    refactor example: high-scores

--- exercises/practice/high-scores/.meta/example.nim
+++ exercises/practice/high-scores/.meta/example.nim
@@ -7,4 +7,5 @@ func personalBest*(scores: openArray[int]): int =
   scores.max
 
 func personalTopThree*(scores: openArray[int]): seq[int] =
-  scores.sorted(order = Descending)[0 .. min(2, scores.high)]
+  result = scores.sorted(order = Descending)
+  result.setLen min(3, scores.len)
