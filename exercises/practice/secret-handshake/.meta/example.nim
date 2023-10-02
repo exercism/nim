b0b582c0d17a739653eaec52a73b178bbc05b60e
ee7 <45465154+ee7@users.noreply.github.com>
2023-10-02 08:00:02 +0200

    exercises(secret-handshake): example: inline use of testBit

--- exercises/practice/secret-handshake/.meta/example.nim
+++ exercises/practice/secret-handshake/.meta/example.nim
@@ -4,15 +4,12 @@ type
   Action* = enum
     Wink, DoubleBlink, CloseEyes, Jump, Reverse
 
-func has(n: Natural, action: Action): bool =
-  testBit(n, action.ord)
-
 func commands*(n: Natural): seq[Action] =
   result = newSeqOfCap[Action](Action.high.ord)
 
   for action in Wink..Jump:
-    if n.has(action):
+    if n.testBit(action.ord):
       result.add action
 
-  if n.has(Reverse):
+  if n.testBit(Reverse.ord):
     reverse result
