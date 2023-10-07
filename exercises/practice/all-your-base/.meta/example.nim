b53ab99d70d57c8af7d51125cc1dce3a59edc3a5
ee7 <45465154+ee7@users.noreply.github.com>
2023-10-07 10:00:02 +0200

    fixup! refactor example: all-your-base
    Simplify, and reduce needless diff size with `main`.

--- exercises/practice/all-your-base/.meta/example.nim
+++ exercises/practice/all-your-base/.meta/example.nim
@@ -2,16 +2,13 @@ import std/algorithm
 
 func toBase10(digits: openArray[int], fromBase: int): int =
   result = 0
-  var power = 1
-  for i in countdown(digits.high, 0):
-    let d = digits[i]
-    result += d * power
-    power *= fromBase
+  for d in digits:
+    result = result * fromBase + d
 
 func fromBase10(n: int, toBase: int): seq[int] =
   if n == 0:
     return @[0]
-  result = newSeq[int]()
+  result = @[]
   var n = n
   while n > 0:
     result.add n mod toBase
@@ -21,10 +18,9 @@ func fromBase10(n: int, toBase: int): seq[int] =
 func convert*(digits: openArray[int], fromBase: int, toBase: int): seq[int] =
   if fromBase < 2:
     raise newException(ValueError, "Invalid input base: " & $fromBase)
-  elif toBase < 2:
+  if toBase < 2:
     raise newException(ValueError, "Invalid output base: " & $toBase)
-  else:
-    for d in digits:
-      if d >= fromBase or d < 0:
-        raise newException(ValueError, "Invalid input digit: " & $d)
+  for d in digits:
+    if d >= fromBase or d < 0:
+      raise newException(ValueError, "Invalid input digit: " & $d)
   digits.toBase10(fromBase).fromBase10(toBase)
