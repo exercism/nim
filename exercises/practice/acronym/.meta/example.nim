629be9c5f12cc84e6dd69d24f77c527dffc029bd
ee7 <45465154+ee7@users.noreply.github.com>
2023-10-07 10:00:01 +0200

    fixup! refactor example: acronym
    This seems slightly faster (at least for the test suite), and is
    shorter.

--- exercises/practice/acronym/.meta/example.nim
+++ exercises/practice/acronym/.meta/example.nim
@@ -2,10 +2,7 @@ import std/strutils
 
 func abbreviate*(s: string): string =
   result = ""
-  var isAfterDelim = true
-  for c in s:
-    if isAfterDelim and c in {'A'..'Z', 'a'..'z'}:
-      result.add c.toUpperAscii()
-      isAfterDelim = false
-    elif c in {' ', '-'}:
-      isAfterDelim = true
+  for i in 0 ..< s.len:
+    if s[i] in {'A'..'Z', 'a'..'z'}:
+      if i == 0 or s[i-1] in {' ', '-', '_'}:
+        result.add s[i].toUpperAscii()
