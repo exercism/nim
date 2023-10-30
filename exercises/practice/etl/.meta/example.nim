4083c4e54bba7135f15f3c63a3beeae4e3858ddd
ee7 <45465154+ee7@users.noreply.github.com>
2023-10-30 08:00:01 +0100

    fixup! refactor example: etl
    Reduce diff size where not particularly meaningful.

--- exercises/practice/etl/.meta/example.nim
+++ exercises/practice/etl/.meta/example.nim
@@ -4,4 +4,4 @@ func transform*(t: Table[int, seq[char]]): CountTable[char] =
   result = initCountTable[char]()
   for score, letters in t:
     for c in letters:
-      result[c.toLowerAscii()] = score
+      result[c.toLowerAscii] = score
