5081576d7fd554e1cdcc83af2f04169688a89f71
ee7 <45465154+ee7@users.noreply.github.com>
2023-11-12 15:50:01 +0100

    fixup! refactor example: all-your-base
    Reduce diff size.

--- exercises/practice/all-your-base/.meta/example.nim
+++ exercises/practice/all-your-base/.meta/example.nim
@@ -1,26 +1,24 @@
 import std/algorithm
 
-func toBase10(digits: openArray[int], fromBase: int): int =
-  result = 0
-  for d in digits:
-    result = result * fromBase + d
-
-func fromBase10(n: int, toBase: int): seq[int] =
-  if n == 0:
-    return @[0]
-  result = @[]
-  var n = n
-  while n > 0:
-    result.add n mod toBase
-    n = n div toBase
-  result.reverse
-
 func convert*(digits: openArray[int], fromBase: int, toBase: int): seq[int] =
   if fromBase < 2:
     raise newException(ValueError, "Invalid input base: " & $fromBase)
   if toBase < 2:
     raise newException(ValueError, "Invalid output base: " & $toBase)
+
+  # Convert to base 10.
+  var n = 0
   for d in digits:
-    if d >= fromBase or d < 0:
+    if d < fromBase and d >= 0:
+      n = n * fromBase + d
+    else:
       raise newException(ValueError, "Invalid input digit: " & $d)
-  digits.toBase10(fromBase).fromBase10(toBase)
+
+  # Convert base 10 to digits in the output base.
+  if n == 0:
+    return @[0]
+  result = @[]
+  while n > 0:
+    result.add n mod toBase
+    n = n div toBase
+  result.reverse
