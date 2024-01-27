e2ade8d73c2933e4815f3b52f7c32e93c13e169e
ee7 <45465154+ee7@users.noreply.github.com>
2024-01-27 18:15:10 +0100

    exercises(robot-simulator): example: switch right and left procs
    Make the order match their usage in `move`.

--- exercises/practice/robot-simulator/.meta/example.nim
+++ exercises/practice/robot-simulator/.meta/example.nim
@@ -10,19 +10,19 @@ type
 func initRobot*(facing = North, x = 0, y = 0): Robot =
   Robot(direction: facing, x: x, y: y)
 
-func right(dir: Compass): Compass =
-  case dir:
-    of North: East
-    of East: South
-    of South: West
-    of West: North
-
 func left(dir: Compass): Compass =
   case dir:
     of North: West
     of East: North
     of South: East
     of West: South
+
+func right(dir: Compass): Compass =
+  case dir:
+    of North: East
+    of East: South
+    of South: West
+    of West: North
 
 func move*(robot: var Robot, instructions: string) =
   for c in instructions:
