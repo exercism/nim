7e70ddaf50b67b7b14faec8b04c6830b60607a40
ee7 <45465154+ee7@users.noreply.github.com>
2024-01-27 18:15:03 +0100

    exercises(robot-simulator): stub: remove comments

--- exercises/practice/robot-simulator/robot_simulator.nim
+++ exercises/practice/robot-simulator/robot_simulator.nim
@@ -7,10 +7,8 @@ type
     xPos*: int
     yPos*: int
 
-# constructor proc to return a new robot with default attributes
 proc initRobot*(facing = North, x = 0, y = 0): Robot =
   discard
 
-# implement this proc to move the robot following the instructions
 proc move*(robot: var Robot, instructions: string) =
   discard
