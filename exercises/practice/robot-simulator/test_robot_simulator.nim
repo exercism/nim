6cea53132b7aa775bce04cdde7ed21dc09c93726
ee7 <45465154+ee7@users.noreply.github.com>
2024-01-27 18:15:14 +0100

    exercises(robot-simulator): tests: lowercase test names
    Be consistent with the other test files on the track.

--- exercises/practice/robot-simulator/test_robot_simulator.nim
+++ exercises/practice/robot-simulator/test_robot_simulator.nim
@@ -2,94 +2,94 @@ import unittest
 import robot_simulator
 
 suite "Robot_Simulator":
-  test "Create robot: at origin facing north":
+  test "create robot: at origin facing north":
     var robot = initRobot()
     check robot.direction == North
 
-  test "Create robot: at negative position facing south":
+  test "create robot: at negative position facing south":
     var robot = initRobot(South, -1, -1)
     check robot.direction == South
     check robot.x == -1
     check robot.y == -1
 
-  test "Rotating clockwise: changes north to east":
+  test "rotating clockwise: changes north to east":
     var robot = initRobot()
     robot.move("R")
     check robot.direction == East
     check robot.x == 0
     check robot.y == 0
 
-  test "Rotating clockwise: changes east to south":
+  test "rotating clockwise: changes east to south":
     var robot = initRobot(East)
     robot.move("R")
     check robot.direction == South
     check robot.x == 0
     check robot.y == 0
 
-  test "Rotating clockwise: changes south to west":
+  test "rotating clockwise: changes south to west":
     var robot = initRobot(South)
     robot.move("R")
     check robot.direction == West
     check robot.x == 0
     check robot.y == 0
 
-  test "Rotating clockwise: changes west to north":
+  test "rotating clockwise: changes west to north":
     var robot = initRobot(West)
     robot.move("R")
     check robot.direction == North
     check robot.x == 0
     check robot.y == 0
 
-  test "Rotating counter-clockwise: changes north to west":
+  test "rotating counter-clockwise: changes north to west":
     var robot = initRobot()
     robot.move("L")
     check robot.direction == West
     check robot.x == 0
     check robot.y == 0
 
-  test "Rotating counter-clockwise: changes west to south":
+  test "rotating counter-clockwise: changes west to south":
     var robot = initRobot(West)
     robot.move("L")
     check robot.direction == South
     check robot.x == 0
     check robot.y == 0
 
-  test "Rotating counter-clockwise: changes south to east":
+  test "rotating counter-clockwise: changes south to east":
     var robot = initRobot(South)
     robot.move("L")
     check robot.direction == East
     check robot.x == 0
     check robot.y == 0
 
-  test "Rotating counter-clockwise: changes east to north":
+  test "rotating counter-clockwise: changes east to north":
     var robot = initRobot(East)
     robot.move("L")
     check robot.direction == North
     check robot.x == 0
     check robot.y == 0
 
-  test "Moving forward one: facing north increments Y":
+  test "moving forward one: facing north increments Y":
     var robot = initRobot()
     robot.move("A")
     check robot.direction == North
     check robot.x == 0
     check robot.y == 1
 
-  test "Moving forward one: facing south decrements Y":
+  test "moving forward one: facing south decrements Y":
     var robot = initRobot(South)
     robot.move("A")
     check robot.direction == South
     check robot.x == 0
     check robot.y == -1
 
-  test "Moving forward one: facing east increments X":
+  test "moving forward one: facing east increments X":
     var robot = initRobot(East)
     robot.move("A")
     check robot.direction == East
     check robot.x == 1
     check robot.y == 0
 
-  test "Moving forward one: facing west decrements X":
+  test "moving forward one: facing west decrements X":
     var robot = initRobot(West)
     robot.move("A")
     check robot.direction == West
