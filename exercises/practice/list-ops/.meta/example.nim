54aa4e70dc7159dff6833c99973c82cded4e4c6a
ee7 <45465154+ee7@users.noreply.github.com>
2024-01-27 14:25:13 +0100

    exercises(list-ops): example: prefer openArray

--- exercises/practice/list-ops/.meta/example.nim
+++ exercises/practice/list-ops/.meta/example.nim
@@ -10,40 +10,40 @@ func append*(list1, list2: seq[int]): seq[int] =
     result[i] = x
     inc i
 
-func concatenate*(lists: seq[seq[int]]): seq[int] =
+func concatenate*(lists: openArray[seq[int]]): seq[int] =
   result = @[]
   for list in lists:
     result = append(result, list)
 
-func filter*(predicate: proc(x: int): bool {.noSideEffect.}, list: seq[int]): seq[int] =
+func filter*(predicate: proc(x: int): bool {.noSideEffect.}, list: openArray[int]): seq[int] =
   result = @[]
   for x in list:
     if predicate(x):
       result.add x
 
-func length*(list: seq[int]): int =
+func length*(list: openArray[int]): int =
   list.len
 
-func map*(function: proc(x: int): int {.noSideEffect.}, list: seq[int]): seq[int] =
+func map*(function: proc(x: int): int {.noSideEffect.}, list: openArray[int]): seq[int] =
   result = newSeq[int](list.len)
   for i, x in list:
     result[i] = function(x)
 
-func foldl*(function: proc(x, y: int): int {.noSideEffect.}, list: seq[int],
+func foldl*(function: proc(x, y: int): int {.noSideEffect.}, list: openArray[int],
             accumulator: int): int =
   if list.len == 0:
     accumulator
   else:
     foldl(function, list[1..^1], function(accumulator, list[0]))
 
-func foldr*(function: proc(x, y: int): int {.noSideEffect.}, list: seq[int],
+func foldr*(function: proc(x, y: int): int {.noSideEffect.}, list: openArray[int],
             accumulator: int): int =
   if list.len == 0:
     accumulator
   else:
     function(foldr(function, list[1..^1], accumulator), list[0])
 
-func reverse*(list: seq[int]): seq[int] =
+func reverse*(list: openArray[int]): seq[int] =
   result = newSeq[int](list.len)
   for i, x in list:
     result[list.high - i] = x
