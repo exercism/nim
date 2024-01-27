26b4b6ed973439468b299f4b71b39d44a6fbf121
ee7 <45465154+ee7@users.noreply.github.com>
2024-01-27 14:25:03 +0100

    exercises(list-ops): example: use result, not newList

--- exercises/practice/list-ops/.meta/example.nim
+++ exercises/practice/list-ops/.meta/example.nim
@@ -4,26 +4,23 @@ proc append*(list1, list2: seq[int]): seq[int] =
   return append(list1 & list2[0], list2[1..^1])
 
 proc concatenate*(lists: seq[seq[int]]): seq[int] =
-  var newList = newSeq[int]()
+  result = @[]
   for list in lists:
-    newList = append(newList, list)
-  return newList
+    result = append(result, list)
 
 proc filter*(predicate: proc(x: int): bool, list: seq[int]): seq[int] =
-  var newList = newSeq[int]()
+  result = @[]
   for x in list:
     if predicate(x):
-      newList.add(x)
-  return newList
+      result.add(x)
 
 proc length*(list: seq[int]): int =
   list.len
 
 proc map*(function: proc(x: int): int, list: seq[int]): seq[int] =
-  var newList = newSeq[int]()
+  result = @[]
   for x in list:
-    newList.add(function(x))
-  return newList
+    result.add(function(x))
 
 proc foldl*(function: proc(x, y: int): int, list: seq[int], accumulator: int): int =
   if list.len == 0:
@@ -36,6 +33,5 @@ proc foldr*(function: proc(x, y: int): int, list: seq[int], accumulator: int): i
   return function(foldr(function, list[1..^1], accumulator), list[0])
 
 proc reverse*(list: seq[int]): seq[int] =
-  var newList = newSeq[int]()
-  for x in list: newList.insert(x, 0)
-  return newList
+  result = @[]
+  for x in list: result.insert(x, 0)
