d4ad759358b14e8cca117d8c24f6a85e4ec6f458
Matthias Rahlf <matthias@webding.de>
2024-02-02 08:23:50 +0100

    knapsack: Fix symbol
    `new_seq` should be `newSeq`

--- exercises/practice/knapsack/.meta/example.nim
+++ exercises/practice/knapsack/.meta/example.nim
@@ -1,7 +1,7 @@
 type Item = tuple[weight: int, value: int]
 
 proc maximumValue*(maximumWeight: int, items: openArray[Item]): int =
-    var dp = new_seq[int](maximumWeight + 1)
+    var dp = newSeq[int](maximumWeight + 1)
     for item in items:
         for weight in countdown(maximumWeight, item.weight):
             dp[weight] = max(dp[weight], item.value + dp[weight - item.weight])
