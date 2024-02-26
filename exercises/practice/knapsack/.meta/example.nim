428e6a90e707b660b0045ce8bd0ce3f85e31df3b
Matthias <matthias@webding.de>
2024-02-26 22:43:13 +0100

    knapsack: fix indentation
    Co-authored-by: ee7 <45465154+ee7@users.noreply.github.com>

--- exercises/practice/knapsack/.meta/example.nim
+++ exercises/practice/knapsack/.meta/example.nim
@@ -2,7 +2,7 @@ type Item = tuple[weight: int, value: int]
 
 proc maximumValue*(maximumWeight: int, items: openArray[Item]): int =
   var dp = newSeq[int](maximumWeight + 1)
-    for item in items:
-        for weight in countdown(maximumWeight, item.weight):
-            dp[weight] = max(dp[weight], item.value + dp[weight - item.weight])
-    result = dp[maximumWeight]
+  for item in items:
+    for weight in countdown(maximumWeight, item.weight):
+      dp[weight] = max(dp[weight], item.value + dp[weight - item.weight])
+  result = dp[maximumWeight]
