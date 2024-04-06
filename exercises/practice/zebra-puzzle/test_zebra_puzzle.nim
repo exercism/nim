316e3e4bb15a6a36cd97f237c0869d90833f4e4b
Erik Schierboom <erik_schierboom@hotmail.com>
2024-04-06 11:03:30 +0200

    Add `zebra-puzzle` exercise

---
+++ exercises/practice/zebra-puzzle/test_zebra_puzzle.nim
@@ -0,0 +1,9 @@
+import unittest
+import zebra_puzzle
+
+suite "Zebra Puzzle":
+  test "resident who drinks water":
+    check waterDrunkBy() == Nationality.Norwegian
+
+  test "resident who owns zebra":
+    check zebraOwnedBy() == Nationality.Japanese
